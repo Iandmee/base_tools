tadfisher/platform_tools_base

Extra shell features from IDevice.
Make RemoteAndroidTestRunner use it instead of the full
interface.

Change-Id: Id95e9013a9011e206f826ab8068f051f393cc34a

ddmlib/src/main/java/com/android/ddmlib/IDevice.java
@@ -24,7 +24,7 @@ import java.util.Map;
 /**
  *  A Device. It can be a physical device or an emulator.
  */
-public interface IDevice {
+public interface IDevice extends ShellEnabledDevice {
 
     public static final String PROP_BUILD_VERSION = "ro.build.version.release";
     public static final String PROP_BUILD_API_LEVEL = "ro.build.version.sdk";
@@ -115,12 +115,6 @@ public interface IDevice {
      * @return the name of the AVD or <code>null</code> if there isn't any.
      */
     public String getAvdName();
-
-    /**
-     * Returns a (humanized) name for this device. Typically this is the AVD name for AVD's, and
-     * a combination of the manufacturer name, model name & serial number for devices.
-     */
-    public String getName();
 
     /**
      * Returns the state of the device.
@@ -285,38 +279,6 @@ public interface IDevice {
      * @see DdmPreferences#getTimeOut()
      */
     public void executeShellCommand(String command, IShellOutputReceiver receiver)
-            throws TimeoutException, AdbCommandRejectedException, ShellCommandUnresponsiveException,
-            IOException;
-
-    /**
-     * Executes a shell command on the device, and sends the result to a <var>receiver</var>.
-     * <p/><var>maxTimeToOutputResponse</var> is used as a maximum waiting time when expecting the
-     * command output from the device.<br>
-     * At any time, if the shell command does not output anything for a period longer than
-     * <var>maxTimeToOutputResponse</var>, then the method will throw
-     * {@link ShellCommandUnresponsiveException}.
-     * <p/>For commands like log output, a <var>maxTimeToOutputResponse</var> value of 0, meaning
-     * that the method will never throw and will block until the receiver's
-     * {@link IShellOutputReceiver#isCancelled()} returns <code>true</code>, should be
-     * used.
-     *
-     * @param command the shell command to execute
-     * @param receiver the {@link IShellOutputReceiver} that will receives the output of the shell
-     *            command
-     * @param maxTimeToOutputResponse the maximum amount of time during which the command is allowed
-     *            to not output any response. A value of 0 means the method will wait forever
-     *            (until the <var>receiver</var> cancels the execution) for command output and
-     *            never throw.
-     * @throws TimeoutException in case of timeout on the connection when sending the command.
-     * @throws AdbCommandRejectedException if adb rejects the command.
-     * @throws ShellCommandUnresponsiveException in case the shell command doesn't send any output
-     *            for a period longer than <var>maxTimeToOutputResponse</var>.
-     * @throws IOException in case of I/O error on the connection.
-     *
-     * @see DdmPreferences#getTimeOut()
-     */
-    public void executeShellCommand(String command, IShellOutputReceiver receiver,
-            int maxTimeToOutputResponse)
             throws TimeoutException, AdbCommandRejectedException, ShellCommandUnresponsiveException,
             IOException;
 

ddmlib/src/main/java/com/android/ddmlib/testrunner/RemoteAndroidTestRunner.java
@@ -18,7 +18,7 @@ package com.android.ddmlib.testrunner;
 
 
 import com.android.ddmlib.AdbCommandRejectedException;
-import com.android.ddmlib.IDevice;
+import com.android.ddmlib.ShellEnabledDevice;
 import com.android.ddmlib.Log;
 import com.android.ddmlib.ShellCommandUnresponsiveException;
 import com.android.ddmlib.TimeoutException;
@@ -37,7 +37,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
 
     private final String mPackageName;
     private final String mRunnerName;
-    private IDevice mRemoteDevice;
+    private ShellEnabledDevice mRemoteDevice;
     // default to no timeout
     private int mMaxTimeToOutputResponse = 0;
     private String mRunName = null;
@@ -71,7 +71,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
      */
     public RemoteAndroidTestRunner(String packageName,
                                    String runnerName,
-                                   IDevice remoteDevice) {
+                                   ShellEnabledDevice remoteDevice) {
 
         mPackageName = packageName;
         mRunnerName = runnerName;
@@ -86,7 +86,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
      * @param remoteDevice the Android device to execute tests on
      */
     public RemoteAndroidTestRunner(String packageName,
-                                   IDevice remoteDevice) {
+                                   ShellEnabledDevice remoteDevice) {
         this(packageName, null, remoteDevice);
     }
 
@@ -203,7 +203,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
         final String runCaseCommandStr = String.format("am instrument -w -r %1$s %2$s",
             getArgsCommand(), getRunnerPath());
         Log.i(LOG_TAG, String.format("Running %1$s on %2$s", runCaseCommandStr,
-                mRemoteDevice.getSerialNumber()));
+                mRemoteDevice.getName()));
         String runName = mRunName == null ? mPackageName : mRunName;
         mParser = new InstrumentationResultParser(runName, listeners);
 
@@ -211,14 +211,14 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
             mRemoteDevice.executeShellCommand(runCaseCommandStr, mParser, mMaxTimeToOutputResponse);
         } catch (IOException e) {
             Log.w(LOG_TAG, String.format("IOException %1$s when running tests %2$s on %3$s",
-                    e.toString(), getPackageName(), mRemoteDevice.getSerialNumber()));
+                    e.toString(), getPackageName(), mRemoteDevice.getName()));
             // rely on parser to communicate results to listeners
             mParser.handleTestRunFailed(e.toString());
             throw e;
         } catch (ShellCommandUnresponsiveException e) {
             Log.w(LOG_TAG, String.format(
                     "ShellCommandUnresponsiveException %1$s when running tests %2$s on %3$s",
-                    e.toString(), getPackageName(), mRemoteDevice.getSerialNumber()));
+                    e.toString(), getPackageName(), mRemoteDevice.getName()));
             mParser.handleTestRunFailed(String.format(
                     "Failed to receive adb shell test output within %1$d ms. " +
                     "Test may have timed out, or adb connection to device became unresponsive",
@@ -227,13 +227,13 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
         } catch (TimeoutException e) {
             Log.w(LOG_TAG, String.format(
                     "TimeoutException when running tests %1$s on %2$s", getPackageName(),
-                    mRemoteDevice.getSerialNumber()));
+                    mRemoteDevice.getName()));
             mParser.handleTestRunFailed(e.toString());
             throw e;
         } catch (AdbCommandRejectedException e) {
             Log.w(LOG_TAG, String.format(
                     "AdbCommandRejectedException %1$s when running tests %2$s on %3$s",
-                    e.toString(), getPackageName(), mRemoteDevice.getSerialNumber()));
+                    e.toString(), getPackageName(), mRemoteDevice.getName()));
             mParser.handleTestRunFailed(e.toString());
             throw e;
         }

Iandmee/base_tools

ddmlib/src/main/java/com/android/ddmlib/IDevice.java
@@ -24,7 +24,7 @@ import java.util.Map;
 /**
  *  A Device. It can be a physical device or an emulator.
  */
-public interface IDevice {
+public interface IDevice extends IShellEnabledDevice {
 
     public static final String PROP_BUILD_VERSION = "ro.build.version.release";
     public static final String PROP_BUILD_API_LEVEL = "ro.build.version.sdk";
@@ -115,12 +115,6 @@ public interface IDevice {
      * @return the name of the AVD or <code>null</code> if there isn't any.
      */
     public String getAvdName();
-
-    /**
-     * Returns a (humanized) name for this device. Typically this is the AVD name for AVD's, and
-     * a combination of the manufacturer name, model name & serial number for devices.
-     */
-    public String getName();
 
     /**
      * Returns the state of the device.
@@ -285,38 +279,6 @@ public interface IDevice {
      * @see DdmPreferences#getTimeOut()
      */
     public void executeShellCommand(String command, IShellOutputReceiver receiver)
-            throws TimeoutException, AdbCommandRejectedException, ShellCommandUnresponsiveException,
-            IOException;
-
-    /**
-     * Executes a shell command on the device, and sends the result to a <var>receiver</var>.
-     * <p/><var>maxTimeToOutputResponse</var> is used as a maximum waiting time when expecting the
-     * command output from the device.<br>
-     * At any time, if the shell command does not output anything for a period longer than
-     * <var>maxTimeToOutputResponse</var>, then the method will throw
-     * {@link ShellCommandUnresponsiveException}.
-     * <p/>For commands like log output, a <var>maxTimeToOutputResponse</var> value of 0, meaning
-     * that the method will never throw and will block until the receiver's
-     * {@link IShellOutputReceiver#isCancelled()} returns <code>true</code>, should be
-     * used.
-     *
-     * @param command the shell command to execute
-     * @param receiver the {@link IShellOutputReceiver} that will receives the output of the shell
-     *            command
-     * @param maxTimeToOutputResponse the maximum amount of time during which the command is allowed
-     *            to not output any response. A value of 0 means the method will wait forever
-     *            (until the <var>receiver</var> cancels the execution) for command output and
-     *            never throw.
-     * @throws TimeoutException in case of timeout on the connection when sending the command.
-     * @throws AdbCommandRejectedException if adb rejects the command.
-     * @throws ShellCommandUnresponsiveException in case the shell command doesn't send any output
-     *            for a period longer than <var>maxTimeToOutputResponse</var>.
-     * @throws IOException in case of I/O error on the connection.
-     *
-     * @see DdmPreferences#getTimeOut()
-     */
-    public void executeShellCommand(String command, IShellOutputReceiver receiver,
-            int maxTimeToOutputResponse)
             throws TimeoutException, AdbCommandRejectedException, ShellCommandUnresponsiveException,
             IOException;
 

ddmlib/src/main/java/com/android/ddmlib/testrunner/RemoteAndroidTestRunner.java
@@ -18,7 +18,7 @@ package com.android.ddmlib.testrunner;
 
 
 import com.android.ddmlib.AdbCommandRejectedException;
-import com.android.ddmlib.IDevice;
+import com.android.ddmlib.IShellEnabledDevice;
 import com.android.ddmlib.Log;
 import com.android.ddmlib.ShellCommandUnresponsiveException;
 import com.android.ddmlib.TimeoutException;
@@ -37,7 +37,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
 
     private final String mPackageName;
     private final String mRunnerName;
-    private IDevice mRemoteDevice;
+    private IShellEnabledDevice mRemoteDevice;
     // default to no timeout
     private int mMaxTimeToOutputResponse = 0;
     private String mRunName = null;
@@ -71,7 +71,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
      */
     public RemoteAndroidTestRunner(String packageName,
                                    String runnerName,
-                                   IDevice remoteDevice) {
+                                   IShellEnabledDevice remoteDevice) {
 
         mPackageName = packageName;
         mRunnerName = runnerName;
@@ -86,7 +86,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
      * @param remoteDevice the Android device to execute tests on
      */
     public RemoteAndroidTestRunner(String packageName,
-                                   IDevice remoteDevice) {
+                                   IShellEnabledDevice remoteDevice) {
         this(packageName, null, remoteDevice);
     }
 
@@ -203,7 +203,7 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
         final String runCaseCommandStr = String.format("am instrument -w -r %1$s %2$s",
             getArgsCommand(), getRunnerPath());
         Log.i(LOG_TAG, String.format("Running %1$s on %2$s", runCaseCommandStr,
-                mRemoteDevice.getSerialNumber()));
+                mRemoteDevice.getName()));
         String runName = mRunName == null ? mPackageName : mRunName;
         mParser = new InstrumentationResultParser(runName, listeners);
 
@@ -211,14 +211,14 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
             mRemoteDevice.executeShellCommand(runCaseCommandStr, mParser, mMaxTimeToOutputResponse);
         } catch (IOException e) {
             Log.w(LOG_TAG, String.format("IOException %1$s when running tests %2$s on %3$s",
-                    e.toString(), getPackageName(), mRemoteDevice.getSerialNumber()));
+                    e.toString(), getPackageName(), mRemoteDevice.getName()));
             // rely on parser to communicate results to listeners
             mParser.handleTestRunFailed(e.toString());
             throw e;
         } catch (ShellCommandUnresponsiveException e) {
             Log.w(LOG_TAG, String.format(
                     "ShellCommandUnresponsiveException %1$s when running tests %2$s on %3$s",
-                    e.toString(), getPackageName(), mRemoteDevice.getSerialNumber()));
+                    e.toString(), getPackageName(), mRemoteDevice.getName()));
             mParser.handleTestRunFailed(String.format(
                     "Failed to receive adb shell test output within %1$d ms. " +
                     "Test may have timed out, or adb connection to device became unresponsive",
@@ -227,13 +227,13 @@ public class RemoteAndroidTestRunner implements IRemoteAndroidTestRunner  {
         } catch (TimeoutException e) {
             Log.w(LOG_TAG, String.format(
                     "TimeoutException when running tests %1$s on %2$s", getPackageName(),
-                    mRemoteDevice.getSerialNumber()));
+                    mRemoteDevice.getName()));
             mParser.handleTestRunFailed(e.toString());
             throw e;
         } catch (AdbCommandRejectedException e) {
             Log.w(LOG_TAG, String.format(
                     "AdbCommandRejectedException %1$s when running tests %2$s on %3$s",
-                    e.toString(), getPackageName(), mRemoteDevice.getSerialNumber()));
+                    e.toString(), getPackageName(), mRemoteDevice.getName()));
             mParser.handleTestRunFailed(e.toString());
             throw e;
         }