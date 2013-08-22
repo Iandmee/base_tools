tadfisher/platform_tools_base

traceview: Show tooltip with entry/exit times on hover
Change-Id: Ib06be9c92817022aeed7fb8d55a3c62f793cc341

perflib/src/main/java/com/android/tools/perflib/vmtrace/viz/TimeScaleRenderer.java
@@ -85,40 +85,17 @@ public class TimeScaleRenderer {
         g2d.drawLine(0, TIMELINE_Y_OFFSET, screenWidth, TIMELINE_Y_OFFSET);
 
         // draw the time at the leftmost end of the screen corresponding to (0,y)
-        String time = humanizeTime(start, end - start, mTimeUnits);
+        String time = TimeUtils.humanizeTime(start, end - start, mTimeUnits);
         g2d.drawString(time,
                 0 + TIMELINE_UNIT_HORIZONTAL_PADDING,
                 TIMELINE_Y_OFFSET - TIMELINE_UNIT_VERTICAL_PADDING);
 
         // draw the time at the leftmost end of the screen corresponding to (screen width,y)
-        time = humanizeTime(end, end - start, mTimeUnits);
+        time = TimeUtils.humanizeTime(end, end - start, mTimeUnits);
         g2d.drawString(time,
                 screenWidth - g2d.getFontMetrics().stringWidth(time)
                         - TIMELINE_UNIT_HORIZONTAL_PADDING,
                 TIMELINE_Y_OFFSET - TIMELINE_UNIT_VERTICAL_PADDING);
-    }
-
-    private String humanizeTime(long time, long span, TimeUnit timeUnits) {
-        String units;
-        double scale;
-        if (timeUnits.toSeconds(span) > 0) {
-            units = "s";
-            scale = 1e-9;
-        } else if (timeUnits.toMillis(span) > 0) {
-            units = "ms";
-            scale = 1e-6;
-        } else {
-            units = "us";
-            scale = 1e-3;
-        }
-
-        return String.format("%1$s %2$s", formatTime(timeUnits.toNanos(time), scale), units);
-    }
-
-    private static final DecimalFormat TIME_FORMATTER = new DecimalFormat("#.###");
-
-    private String formatTime(long nsecs, double scale) {
-        return TIME_FORMATTER.format(nsecs * scale);
     }
 
     public int getLayoutHeight() {

perflib/src/main/java/com/android/tools/perflib/vmtrace/viz/TraceViewCanvas.java
@@ -22,8 +22,9 @@ import com.android.tools.perflib.vmtrace.VmTraceData;
 import com.android.utils.SparseArray;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
-import java.util.concurrent.TimeUnit;
+import java.awt.geom.NoninvertibleTransformException;
 
 import javax.swing.*;
 
@@ -35,6 +36,8 @@ import javax.swing.*;
  */
 public class TraceViewCanvas extends JComponent {
     private static final Color BACKGROUND_COLOR = Color.LIGHT_GRAY;
+
+    private static final int TOOLTIP_OFFSET = 10;
 
     /**
      * Interactor that listens to mouse events, interprets them as zoom/pan events, and provides
@@ -51,10 +54,15 @@ public class TraceViewCanvas extends JComponent {
     /** Combined {@link #mViewPortTransform} * {@link #mScreenTransform}. */
     private AffineTransform mTransform;
 
+    /** Inverse of {@link #mTransform}. */
+    private AffineTransform mInverseTransform;
+
     private VmTraceData mTraceData;
 
     private TimeScaleRenderer mTimeScaleRenderer;
     private CallHierarchyRenderer mCallHierarchyRenderer;
+
+    private final Point mTmpPoint = new Point();
 
     public TraceViewCanvas() {
         mScreenTransform = new AffineTransform();
@@ -72,6 +80,8 @@ public class TraceViewCanvas extends JComponent {
                 updateViewPortTransform(transform);
             }
         });
+
+        addMouseMotionListener(ToolTipManager.sharedInstance());
     }
 
     public void setTrace(@NonNull VmTraceData traceData, @NonNull String threadName) {
@@ -168,6 +178,18 @@ public class TraceViewCanvas extends JComponent {
         updateTransform();
     }
 
+    @Override
+    public String getToolTipText(MouseEvent event) {
+        mTmpPoint.setLocation(event.getPoint());
+        mInverseTransform.transform(mTmpPoint, mTmpPoint);
+        return mCallHierarchyRenderer.getToolTipFor(mTmpPoint.x, mTmpPoint.y);
+    }
+
+    @Override
+    public Point getToolTipLocation(MouseEvent event) {
+        return new Point(event.getX() + TOOLTIP_OFFSET, event.getY() + TOOLTIP_OFFSET);
+    }
+
     private void updateViewPortTransform(AffineTransform tx) {
         mViewPortTransform = new AffineTransform(tx);
         updateTransform();
@@ -176,6 +198,13 @@ public class TraceViewCanvas extends JComponent {
     private void updateTransform() {
         mTransform = new AffineTransform(mScreenTransform);
         mTransform.concatenate(mViewPortTransform);
+
+        try {
+            mInverseTransform = mViewPortTransform.createInverse();
+        } catch (NoninvertibleTransformException e) {
+            // This can't occur since we just do scale or pan, both of which are invertible
+            mInverseTransform = new AffineTransform();
+        }
 
         repaint();
     }

Iandmee/base_tools

perflib/src/main/java/com/android/tools/perflib/vmtrace/viz/TimeScaleRenderer.java
@@ -19,7 +19,6 @@ package com.android.tools.perflib.vmtrace.viz;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
-import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
 
 public class TimeScaleRenderer {
@@ -85,40 +84,17 @@ public class TimeScaleRenderer {
         g2d.drawLine(0, TIMELINE_Y_OFFSET, screenWidth, TIMELINE_Y_OFFSET);
 
         // draw the time at the leftmost end of the screen corresponding to (0,y)
-        String time = humanizeTime(start, end - start, mTimeUnits);
+        String time = TimeUtils.makeHumanReadable(start, end - start, mTimeUnits);
         g2d.drawString(time,
                 0 + TIMELINE_UNIT_HORIZONTAL_PADDING,
                 TIMELINE_Y_OFFSET - TIMELINE_UNIT_VERTICAL_PADDING);
 
         // draw the time at the leftmost end of the screen corresponding to (screen width,y)
-        time = humanizeTime(end, end - start, mTimeUnits);
+        time = TimeUtils.makeHumanReadable(end, end - start, mTimeUnits);
         g2d.drawString(time,
                 screenWidth - g2d.getFontMetrics().stringWidth(time)
                         - TIMELINE_UNIT_HORIZONTAL_PADDING,
                 TIMELINE_Y_OFFSET - TIMELINE_UNIT_VERTICAL_PADDING);
-    }
-
-    private String humanizeTime(long time, long span, TimeUnit timeUnits) {
-        String units;
-        double scale;
-        if (timeUnits.toSeconds(span) > 0) {
-            units = "s";
-            scale = 1e-9;
-        } else if (timeUnits.toMillis(span) > 0) {
-            units = "ms";
-            scale = 1e-6;
-        } else {
-            units = "us";
-            scale = 1e-3;
-        }
-
-        return String.format("%1$s %2$s", formatTime(timeUnits.toNanos(time), scale), units);
-    }
-
-    private static final DecimalFormat TIME_FORMATTER = new DecimalFormat("#.###");
-
-    private String formatTime(long nsecs, double scale) {
-        return TIME_FORMATTER.format(nsecs * scale);
     }
 
     public int getLayoutHeight() {

perflib/src/main/java/com/android/tools/perflib/vmtrace/viz/TraceViewCanvas.java
@@ -22,8 +22,9 @@ import com.android.tools.perflib.vmtrace.VmTraceData;
 import com.android.utils.SparseArray;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
-import java.util.concurrent.TimeUnit;
+import java.awt.geom.NoninvertibleTransformException;
 
 import javax.swing.*;
 
@@ -35,6 +36,8 @@ import javax.swing.*;
  */
 public class TraceViewCanvas extends JComponent {
     private static final Color BACKGROUND_COLOR = Color.LIGHT_GRAY;
+
+    private static final int TOOLTIP_OFFSET = 10;
 
     /**
      * Interactor that listens to mouse events, interprets them as zoom/pan events, and provides
@@ -51,10 +54,15 @@ public class TraceViewCanvas extends JComponent {
     /** Combined {@link #mViewPortTransform} * {@link #mScreenTransform}. */
     private AffineTransform mTransform;
 
+    /** Inverse of {@link #mViewPortTransform}. */
+    private AffineTransform mViewPortInverseTransform;
+
     private VmTraceData mTraceData;
 
     private TimeScaleRenderer mTimeScaleRenderer;
     private CallHierarchyRenderer mCallHierarchyRenderer;
+
+    private final Point mTmpPoint = new Point();
 
     public TraceViewCanvas() {
         mScreenTransform = new AffineTransform();
@@ -72,6 +80,8 @@ public class TraceViewCanvas extends JComponent {
                 updateViewPortTransform(transform);
             }
         });
+
+        addMouseMotionListener(ToolTipManager.sharedInstance());
     }
 
     public void setTrace(@NonNull VmTraceData traceData, @NonNull String threadName) {
@@ -168,15 +178,34 @@ public class TraceViewCanvas extends JComponent {
         updateTransform();
     }
 
+    @Override
+    public String getToolTipText(MouseEvent event) {
+        mTmpPoint.setLocation(event.getPoint());
+        mViewPortInverseTransform.transform(mTmpPoint, mTmpPoint);
+        return mCallHierarchyRenderer.getToolTipFor(mTmpPoint.x, mTmpPoint.y);
+    }
+
+    @Override
+    public Point getToolTipLocation(MouseEvent event) {
+        return new Point(event.getX() + TOOLTIP_OFFSET, event.getY() + TOOLTIP_OFFSET);
+    }
+
     private void updateViewPortTransform(AffineTransform tx) {
         mViewPortTransform = new AffineTransform(tx);
+
+        try {
+            mViewPortInverseTransform = mViewPortTransform.createInverse();
+        } catch (NoninvertibleTransformException e) {
+            // This can't occur since we just do scale or pan, both of which are invertible
+            mViewPortInverseTransform = new AffineTransform();
+        }
+
         updateTransform();
     }
 
     private void updateTransform() {
         mTransform = new AffineTransform(mScreenTransform);
         mTransform.concatenate(mViewPortTransform);
-
         repaint();
     }
 }