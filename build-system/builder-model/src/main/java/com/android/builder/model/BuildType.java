tadfisher/platform_tools_base

Embedded support
Change-Id: Ie7c85273fa2a716b1d74d1547c8b94bea9cb114d

build-system/builder-model/src/main/java/com/android/builder/model/BuildType.java
@@ -112,4 +112,9 @@ public interface BuildType extends BaseConfig {
      */
     @Nullable
     NdkConfig getNdkConfig();
+
+    /**
+     * Returns whether the variant embeds the micro app.
+     */
+    boolean getEmbedMicroApp();
 }

build-system/builder/src/main/java/com/android/builder/DefaultBuildType.java
@@ -37,6 +37,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
     private String mVersionNameSuffix = null;
     private boolean mRunProguard = false;
     private SigningConfig mSigningConfig = null;
+    private boolean mEmbedMicroApp = true;
 
     private boolean mZipAlign = true;
 
@@ -57,6 +58,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
         setRunProguard(that.isRunProguard());
         setZipAlign(that.isZipAlign());
         setSigningConfig(that.getSigningConfig());
+        setEmbedMicroApp(that.getEmbedMicroApp());
 
         return this;
     }
@@ -194,6 +196,15 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
     }
 
     @Override
+    public boolean getEmbedMicroApp() {
+        return mEmbedMicroApp;
+    }
+
+    public void setEmbedMicroApp(boolean embedMicroApp) {
+        mEmbedMicroApp = embedMicroApp;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -221,6 +232,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
                 !mSigningConfig.equals(buildType.mSigningConfig) :
                 buildType.mSigningConfig != null)
             return false;
+        if (mEmbedMicroApp != buildType.mEmbedMicroApp) return false;
 
         return true;
     }
@@ -239,6 +251,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
         result = 31 * result + (mRunProguard ? 1 : 0);
         result = 31 * result + (mZipAlign ? 1 : 0);
         result = 31 * result + (mSigningConfig != null ? mSigningConfig.hashCode() : 0);
+        result = 31 * result + (mEmbedMicroApp ? 1 : 0);
         return result;
     }
 
@@ -257,6 +270,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
                 .add("runProguard", mRunProguard)
                 .add("zipAlign", mZipAlign)
                 .add("signingConfig", mSigningConfig)
+                .add("embedMicroApp", mEmbedMicroApp)
                 .toString();
     }
 }

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/BuildTypeImpl.java
@@ -45,6 +45,7 @@ class BuildTypeImpl implements BuildType, Serializable {
     private String versionNameSuffix;
     private boolean runProguard;
     private boolean zipAlign;
+    private boolean embedMicroApp;
 
     @NonNull
     static BuildTypeImpl cloneBuildType(BuildType buildType) {
@@ -59,6 +60,7 @@ class BuildTypeImpl implements BuildType, Serializable {
         clonedBuildType.versionNameSuffix = buildType.getVersionNameSuffix();
         clonedBuildType.runProguard = buildType.isRunProguard();
         clonedBuildType.zipAlign = buildType.isZipAlign();
+        clonedBuildType.embedMicroApp = buildType.getEmbedMicroApp();
 
         return clonedBuildType;
     }
@@ -147,5 +149,10 @@ class BuildTypeImpl implements BuildType, Serializable {
     @Nullable
     public NdkConfig getNdkConfig() {
         return null;
+    }
+
+    @Override
+    public boolean getEmbedMicroApp() {
+        return embedMicroApp;
     }
 }

Iandmee/base_tools

build-system/builder-model/src/main/java/com/android/builder/model/BuildType.java
@@ -112,4 +112,9 @@ public interface BuildType extends BaseConfig {
      */
     @Nullable
     NdkConfig getNdkConfig();
+
+    /**
+     * Returns whether the variant embeds the micro app.
+     */
+    boolean isEmbedMicroApp();
 }

build-system/builder/src/main/java/com/android/builder/DefaultBuildType.java
@@ -37,6 +37,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
     private String mVersionNameSuffix = null;
     private boolean mRunProguard = false;
     private SigningConfig mSigningConfig = null;
+    private boolean mEmbedMicroApp = true;
 
     private boolean mZipAlign = true;
 
@@ -57,6 +58,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
         setRunProguard(that.isRunProguard());
         setZipAlign(that.isZipAlign());
         setSigningConfig(that.getSigningConfig());
+        setEmbedMicroApp(that.isEmbedMicroApp());
 
         return this;
     }
@@ -194,6 +196,15 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
     }
 
     @Override
+    public boolean isEmbedMicroApp() {
+        return mEmbedMicroApp;
+    }
+
+    public void setEmbedMicroApp(boolean embedMicroApp) {
+        mEmbedMicroApp = embedMicroApp;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -221,6 +232,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
                 !mSigningConfig.equals(buildType.mSigningConfig) :
                 buildType.mSigningConfig != null)
             return false;
+        if (mEmbedMicroApp != buildType.mEmbedMicroApp) return false;
 
         return true;
     }
@@ -239,6 +251,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
         result = 31 * result + (mRunProguard ? 1 : 0);
         result = 31 * result + (mZipAlign ? 1 : 0);
         result = 31 * result + (mSigningConfig != null ? mSigningConfig.hashCode() : 0);
+        result = 31 * result + (mEmbedMicroApp ? 1 : 0);
         return result;
     }
 
@@ -257,6 +270,7 @@ public class DefaultBuildType extends BaseConfigImpl implements BuildType {
                 .add("runProguard", mRunProguard)
                 .add("zipAlign", mZipAlign)
                 .add("signingConfig", mSigningConfig)
+                .add("embedMicroApp", mEmbedMicroApp)
                 .toString();
     }
 }

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/BuildTypeImpl.java
@@ -45,6 +45,7 @@ class BuildTypeImpl implements BuildType, Serializable {
     private String versionNameSuffix;
     private boolean runProguard;
     private boolean zipAlign;
+    private boolean embedMicroApp;
 
     @NonNull
     static BuildTypeImpl cloneBuildType(BuildType buildType) {
@@ -59,6 +60,7 @@ class BuildTypeImpl implements BuildType, Serializable {
         clonedBuildType.versionNameSuffix = buildType.getVersionNameSuffix();
         clonedBuildType.runProguard = buildType.isRunProguard();
         clonedBuildType.zipAlign = buildType.isZipAlign();
+        clonedBuildType.embedMicroApp = buildType.isEmbedMicroApp();
 
         return clonedBuildType;
     }
@@ -147,5 +149,10 @@ class BuildTypeImpl implements BuildType, Serializable {
     @Nullable
     public NdkConfig getNdkConfig() {
         return null;
+    }
+
+    @Override
+    public boolean isEmbedMicroApp() {
+        return embedMicroApp;
     }
 }