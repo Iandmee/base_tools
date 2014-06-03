tadfisher/platform_tools_base

added a library specific behaviors in the ManifestMerger.
We used to invoke the full featured manifest merger when building flavored
lib which merged the transitive set of dependencies inside the resulting
library manifest file.

This could create issues down the road so instead, we now just invoke a
simple subset of the manifest merger capabilities to do placeholder
substitution and system property injection.

Also library AndroidManifest will not have their tools: annotations
stripped for correct consumption in applications.

Change-Id: I3724b31ad18c16af475ee97a7dde5ea5dc78c35a

build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -83,7 +83,8 @@ import com.android.build.gradle.tasks.PackageApplication
 import com.android.build.gradle.tasks.PreDex
 import com.android.build.gradle.tasks.ProcessAndroidResources
 import com.android.build.gradle.tasks.ProcessAppManifest
-import com.android.build.gradle.tasks.ProcessAppManifest2
+import com.android.build.gradle.tasks.MergeManifests
+import com.android.build.gradle.tasks.ProcessManifest
 import com.android.build.gradle.tasks.ProcessTestManifest
 import com.android.build.gradle.tasks.ProcessTestManifest2
 import com.android.build.gradle.tasks.RenderscriptCompile
@@ -495,7 +496,7 @@ public abstract class BasePlugin {
         }
     }
 
-    public void createProcessManifestTask(BaseVariantData variantData,
+    public void mergeManifests(BaseVariantData variantData,
                                           String manifestOutDir) {
         if (extension.getUseOldManifestMerger()) {
             createOldProcessManifestTask(variantData, manifestOutDir);
@@ -505,8 +506,8 @@ public abstract class BasePlugin {
 
         def processManifestTask = project.tasks.create(
                 "process${variantData.variantConfiguration.fullName.capitalize()}Manifest",
-                ProcessAppManifest2)
-        variantData.processManifestTask = processManifestTask
+                MergeManifests)
+        variantData.mManifestProcessorTask = processManifestTask
         processManifestTask.plugin = this
 
         processManifestTask.dependsOn variantData.prepareDependenciesTask
@@ -548,14 +549,54 @@ public abstract class BasePlugin {
         }
     }
 
+    public void processManifest(BaseVariantData variantData, String manifestOutDir) {
+        if (extension.getUseOldManifestMerger()) {
+            createOldProcessManifestTask(variantData, manifestOutDir);
+            return;
+        }
+        VariantConfiguration config = variantData.variantConfiguration
+
+        def injectLibraryManifest = project.tasks.create(
+                "process${variantData.variantConfiguration.fullName.capitalize()}Manifest",
+                ProcessManifest)
+        variantData.mManifestProcessorTask = injectLibraryManifest
+        injectLibraryManifest.plugin = this
+
+        injectLibraryManifest.dependsOn variantData.prepareDependenciesTask
+        injectLibraryManifest.variantConfiguration = config
+
+        ProductFlavor mergedFlavor = config.mergedFlavor
+
+        injectLibraryManifest.conventionMapping.minSdkVersion = {
+            if (androidBuilder.isPreviewTarget()) {
+                return androidBuilder.getTargetCodename()
+            }
+            return mergedFlavor.minSdkVersion?.apiString
+        }
+
+        injectLibraryManifest.conventionMapping.targetSdkVersion = {
+            if (androidBuilder.isPreviewTarget()) {
+                return androidBuilder.getTargetCodename()
+            }
+
+            return mergedFlavor.targetSdkVersion?.apiString
+        }
+
+        injectLibraryManifest.conventionMapping.manifestOutputFile = {
+            project.file(
+                    "$project.buildDir/${FD_INTERMEDIATES}/${manifestOutDir}/" +
+                            "${variantData.variantConfiguration.dirName}/AndroidManifest.xml")
+        }
+    }
+
     public void createOldProcessManifestTask(BaseVariantData variantData,
             String manifestOurDir) {
         VariantConfiguration config = variantData.variantConfiguration
 
         def processManifestTask = project.tasks.create(
-                "process${variantData.variantConfiguration.fullName.capitalize()}Manifest",
+                "merge${variantData.variantConfiguration.fullName.capitalize()}Manifests",
                 ProcessAppManifest)
-        variantData.processManifestTask = processManifestTask
+        variantData.mManifestProcessorTask = processManifestTask
         processManifestTask.dependsOn variantData.prepareDependenciesTask
         if (config.type != TEST) {
             processManifestTask.dependsOn variantData.checkManifestTask
@@ -617,7 +658,7 @@ public abstract class BasePlugin {
                     ProcessTestManifest2)
         }
 
-        variantData.processManifestTask = processTestManifestTask
+        variantData.mManifestProcessorTask = processTestManifestTask
         processTestManifestTask.dependsOn variantData.prepareDependenciesTask
 
         processTestManifestTask.plugin = this
@@ -670,7 +711,7 @@ public abstract class BasePlugin {
                 RenderscriptCompile)
         variantData.renderscriptCompileTask = renderscriptTask
         if (config.type == TEST) {
-            renderscriptTask.dependsOn variantData.processManifestTask
+            renderscriptTask.dependsOn variantData.mManifestProcessorTask
         } else {
             renderscriptTask.dependsOn variantData.checkManifestTask
         }
@@ -811,7 +852,7 @@ public abstract class BasePlugin {
         if (variantConfiguration.type == TEST) {
             // in case of a test project, the manifest is generated so we need to depend
             // on its creation.
-            generateBuildConfigTask.dependsOn variantData.processManifestTask
+            generateBuildConfigTask.dependsOn variantData.mManifestProcessorTask
         } else {
             generateBuildConfigTask.dependsOn variantData.checkManifestTask
         }
@@ -897,7 +938,7 @@ public abstract class BasePlugin {
         variantData.processResourcesTask = processResources
 
         variantData.sourceGenTask.dependsOn processResources
-        processResources.dependsOn variantData.processManifestTask, variantData.mergeResourcesTask, variantData.mergeAssetsTask
+        processResources.dependsOn variantData.mManifestProcessorTask, variantData.mergeResourcesTask, variantData.mergeAssetsTask
 
         processResources.plugin = this
         processResources.enforceUniquePackageName = extension.getEnforceUniquePackageName()
@@ -905,7 +946,7 @@ public abstract class BasePlugin {
         VariantConfiguration variantConfiguration = variantData.variantConfiguration
 
         processResources.conventionMapping.manifestFile = {
-            variantData.processManifestTask.manifestOutputFile
+            variantData.mManifestProcessorTask.manifestOutputFile
         }
 
         processResources.conventionMapping.resDir = {
@@ -1173,7 +1214,7 @@ public abstract class BasePlugin {
         if (testedVariantData.variantConfiguration.type == VariantConfiguration.Type.LIBRARY) {
             // in this case the tested library must be fully built before test can be built!
             if (testedVariantData.assembleTask != null) {
-                variantData.processManifestTask.dependsOn testedVariantData.assembleTask
+                variantData.mManifestProcessorTask.dependsOn testedVariantData.assembleTask
                 variantData.mergeResourcesTask.dependsOn testedVariantData.assembleTask
             }
         }

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/api/BaseVariantImpl.java
@@ -26,7 +26,7 @@ import com.android.build.gradle.tasks.MergeAssets;
 import com.android.build.gradle.tasks.MergeResources;
 import com.android.build.gradle.tasks.NdkCompile;
 import com.android.build.gradle.tasks.ProcessAndroidResources;
-import com.android.build.gradle.tasks.ProcessManifest;
+import com.android.build.gradle.tasks.ManifestProcessorTask;
 import com.android.build.gradle.tasks.RenderscriptCompile;
 import com.android.builder.core.DefaultBuildType;
 import com.android.builder.core.DefaultProductFlavor;
@@ -126,8 +126,8 @@ abstract class BaseVariantImpl implements BaseVariant {
 
     @Override
     @NonNull
-    public ProcessManifest getProcessManifest() {
-        return getVariantData().processManifestTask;
+    public ManifestProcessorTask getProcessManifest() {
+        return getVariantData().mManifestProcessorTask;
     }
 
     @Override

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/ModelBuilder.groovy
@@ -253,7 +253,7 @@ public class ModelBuilder implements ToolingModelBuilder {
                 vC.packageName,
                 variantData.sourceGenTask.name,
                 variantData.javaCompileTask.name,
-                variantData.processManifestTask.manifestOutputFile,
+                variantData.mManifestProcessorTask.manifestOutputFile,
                 getGeneratedSourceFolders(variantData),
                 getGeneratedResourceFolders(variantData),
                 variantData.javaCompileTask.destinationDir,

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/ApplicationVariantFactory.java
@@ -85,7 +85,7 @@ public class ApplicationVariantFactory implements VariantFactory {
         handleMicroApp(variantData);
 
         // Add a task to process the manifest(s)
-        basePlugin.createProcessManifestTask(variantData, "manifests");
+        basePlugin.mergeManifests(variantData, "manifests");
 
         // Add a task to create the res values
         basePlugin.createGenerateResValuesTask(variantData);

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/BaseVariantData.java
@@ -31,7 +31,7 @@ import com.android.build.gradle.tasks.MergeAssets;
 import com.android.build.gradle.tasks.MergeResources;
 import com.android.build.gradle.tasks.NdkCompile;
 import com.android.build.gradle.tasks.ProcessAndroidResources;
-import com.android.build.gradle.tasks.ProcessManifest;
+import com.android.build.gradle.tasks.ManifestProcessorTask;
 import com.android.build.gradle.tasks.RenderscriptCompile;
 import com.android.builder.core.VariantConfiguration;
 import com.android.builder.model.SourceProvider;
@@ -63,7 +63,7 @@ public abstract class BaseVariantData {
     public Task assetGenTask;
     public CheckManifest checkManifestTask;
 
-    public ProcessManifest processManifestTask;
+    public ManifestProcessorTask mManifestProcessorTask;
     public RenderscriptCompile renderscriptCompileTask;
     public AidlCompile aidlCompileTask;
     public MergeResources mergeResourcesTask;

Iandmee/base_tools

build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -82,7 +82,8 @@ import com.android.build.gradle.tasks.PackageApplication
 import com.android.build.gradle.tasks.PreDex
 import com.android.build.gradle.tasks.ProcessAndroidResources
 import com.android.build.gradle.tasks.ProcessAppManifest
-import com.android.build.gradle.tasks.ProcessAppManifest2
+import com.android.build.gradle.tasks.MergeManifests
+import com.android.build.gradle.tasks.ProcessManifest
 import com.android.build.gradle.tasks.ProcessTestManifest
 import com.android.build.gradle.tasks.ProcessTestManifest2
 import com.android.build.gradle.tasks.RenderscriptCompile
@@ -494,7 +495,7 @@ public abstract class BasePlugin {
         }
     }
 
-    public void createProcessManifestTask(BaseVariantData variantData,
+    public void createMergeManifestsTask(BaseVariantData variantData,
                                           String manifestOutDir) {
         if (extension.getUseOldManifestMerger()) {
             createOldProcessManifestTask(variantData, manifestOutDir);
@@ -504,8 +505,8 @@ public abstract class BasePlugin {
 
         def processManifestTask = project.tasks.create(
                 "process${variantData.variantConfiguration.fullName.capitalize()}Manifest",
-                ProcessAppManifest2)
-        variantData.processManifestTask = processManifestTask
+                MergeManifests)
+        variantData.manifestProcessorTask = processManifestTask
         processManifestTask.plugin = this
 
         processManifestTask.dependsOn variantData.prepareDependenciesTask
@@ -547,14 +548,54 @@ public abstract class BasePlugin {
         }
     }
 
+    public void createProcessManifestTask(BaseVariantData variantData, String manifestOutDir) {
+        if (extension.getUseOldManifestMerger()) {
+            createOldProcessManifestTask(variantData, manifestOutDir);
+            return;
+        }
+        VariantConfiguration config = variantData.variantConfiguration
+
+        def processManifest = project.tasks.create(
+                "process${variantData.variantConfiguration.fullName.capitalize()}Manifest",
+                ProcessManifest)
+        variantData.manifestProcessorTask = processManifest
+        processManifest.plugin = this
+
+        processManifest.dependsOn variantData.prepareDependenciesTask
+        processManifest.variantConfiguration = config
+
+        ProductFlavor mergedFlavor = config.mergedFlavor
+
+        processManifest.conventionMapping.minSdkVersion = {
+            if (androidBuilder.isPreviewTarget()) {
+                return androidBuilder.getTargetCodename()
+            }
+            return mergedFlavor.minSdkVersion?.apiString
+        }
+
+        processManifest.conventionMapping.targetSdkVersion = {
+            if (androidBuilder.isPreviewTarget()) {
+                return androidBuilder.getTargetCodename()
+            }
+
+            return mergedFlavor.targetSdkVersion?.apiString
+        }
+
+        processManifest.conventionMapping.manifestOutputFile = {
+            project.file(
+                    "$project.buildDir/${FD_INTERMEDIATES}/${manifestOutDir}/" +
+                            "${variantData.variantConfiguration.dirName}/AndroidManifest.xml")
+        }
+    }
+
     public void createOldProcessManifestTask(BaseVariantData variantData,
             String manifestOurDir) {
         VariantConfiguration config = variantData.variantConfiguration
 
         def processManifestTask = project.tasks.create(
-                "process${variantData.variantConfiguration.fullName.capitalize()}Manifest",
+                "merge${variantData.variantConfiguration.fullName.capitalize()}Manifests",
                 ProcessAppManifest)
-        variantData.processManifestTask = processManifestTask
+        variantData.manifestProcessorTask = processManifestTask
         processManifestTask.dependsOn variantData.prepareDependenciesTask
         if (config.type != TEST) {
             processManifestTask.dependsOn variantData.checkManifestTask
@@ -616,7 +657,7 @@ public abstract class BasePlugin {
                     ProcessTestManifest2)
         }
 
-        variantData.processManifestTask = processTestManifestTask
+        variantData.manifestProcessorTask = processTestManifestTask
         processTestManifestTask.dependsOn variantData.prepareDependenciesTask
 
         processTestManifestTask.plugin = this
@@ -669,7 +710,7 @@ public abstract class BasePlugin {
                 RenderscriptCompile)
         variantData.renderscriptCompileTask = renderscriptTask
         if (config.type == TEST) {
-            renderscriptTask.dependsOn variantData.processManifestTask
+            renderscriptTask.dependsOn variantData.manifestProcessorTask
         } else {
             renderscriptTask.dependsOn variantData.checkManifestTask
         }
@@ -810,7 +851,7 @@ public abstract class BasePlugin {
         if (variantConfiguration.type == TEST) {
             // in case of a test project, the manifest is generated so we need to depend
             // on its creation.
-            generateBuildConfigTask.dependsOn variantData.processManifestTask
+            generateBuildConfigTask.dependsOn variantData.manifestProcessorTask
         } else {
             generateBuildConfigTask.dependsOn variantData.checkManifestTask
         }
@@ -896,7 +937,7 @@ public abstract class BasePlugin {
         variantData.processResourcesTask = processResources
 
         variantData.sourceGenTask.dependsOn processResources
-        processResources.dependsOn variantData.processManifestTask, variantData.mergeResourcesTask, variantData.mergeAssetsTask
+        processResources.dependsOn variantData.manifestProcessorTask, variantData.mergeResourcesTask, variantData.mergeAssetsTask
 
         processResources.plugin = this
         processResources.enforceUniquePackageName = extension.getEnforceUniquePackageName()
@@ -904,7 +945,7 @@ public abstract class BasePlugin {
         VariantConfiguration variantConfiguration = variantData.variantConfiguration
 
         processResources.conventionMapping.manifestFile = {
-            variantData.processManifestTask.manifestOutputFile
+            variantData.manifestProcessorTask.manifestOutputFile
         }
 
         processResources.conventionMapping.resDir = {
@@ -1172,7 +1213,7 @@ public abstract class BasePlugin {
         if (testedVariantData.variantConfiguration.type == VariantConfiguration.Type.LIBRARY) {
             // in this case the tested library must be fully built before test can be built!
             if (testedVariantData.assembleTask != null) {
-                variantData.processManifestTask.dependsOn testedVariantData.assembleTask
+                variantData.manifestProcessorTask.dependsOn testedVariantData.assembleTask
                 variantData.mergeResourcesTask.dependsOn testedVariantData.assembleTask
             }
         }

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/api/BaseVariantImpl.java
@@ -26,7 +26,7 @@ import com.android.build.gradle.tasks.MergeAssets;
 import com.android.build.gradle.tasks.MergeResources;
 import com.android.build.gradle.tasks.NdkCompile;
 import com.android.build.gradle.tasks.ProcessAndroidResources;
-import com.android.build.gradle.tasks.ProcessManifest;
+import com.android.build.gradle.tasks.ManifestProcessorTask;
 import com.android.build.gradle.tasks.RenderscriptCompile;
 import com.android.builder.core.DefaultBuildType;
 import com.android.builder.core.DefaultProductFlavor;
@@ -126,8 +126,8 @@ abstract class BaseVariantImpl implements BaseVariant {
 
     @Override
     @NonNull
-    public ProcessManifest getProcessManifest() {
-        return getVariantData().processManifestTask;
+    public ManifestProcessorTask getProcessManifest() {
+        return getVariantData().manifestProcessorTask;
     }
 
     @Override

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/ModelBuilder.groovy
@@ -253,7 +253,7 @@ public class ModelBuilder implements ToolingModelBuilder {
                 vC.packageName,
                 variantData.sourceGenTask.name,
                 variantData.javaCompileTask.name,
-                variantData.processManifestTask.manifestOutputFile,
+                variantData.manifestProcessorTask.manifestOutputFile,
                 getGeneratedSourceFolders(variantData),
                 getGeneratedResourceFolders(variantData),
                 variantData.javaCompileTask.destinationDir,

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/ApplicationVariantFactory.java
@@ -85,7 +85,7 @@ public class ApplicationVariantFactory implements VariantFactory {
         handleMicroApp(variantData);
 
         // Add a task to process the manifest(s)
-        basePlugin.createProcessManifestTask(variantData, "manifests");
+        basePlugin.createMergeManifestsTask(variantData, "manifests");
 
         // Add a task to create the res values
         basePlugin.createGenerateResValuesTask(variantData);

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/BaseVariantData.java
@@ -31,7 +31,7 @@ import com.android.build.gradle.tasks.MergeAssets;
 import com.android.build.gradle.tasks.MergeResources;
 import com.android.build.gradle.tasks.NdkCompile;
 import com.android.build.gradle.tasks.ProcessAndroidResources;
-import com.android.build.gradle.tasks.ProcessManifest;
+import com.android.build.gradle.tasks.ManifestProcessorTask;
 import com.android.build.gradle.tasks.RenderscriptCompile;
 import com.android.builder.core.VariantConfiguration;
 import com.android.builder.model.SourceProvider;
@@ -63,7 +63,7 @@ public abstract class BaseVariantData {
     public Task assetGenTask;
     public CheckManifest checkManifestTask;
 
-    public ProcessManifest processManifestTask;
+    public ManifestProcessorTask manifestProcessorTask;
     public RenderscriptCompile renderscriptCompileTask;
     public AidlCompile aidlCompileTask;
     public MergeResources mergeResourcesTask;