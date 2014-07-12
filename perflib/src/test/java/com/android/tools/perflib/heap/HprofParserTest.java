tadfisher/platform_tools_base

Adds tests for object creation and fixes array allocation.
Adds a test for the creation of an Enum class which covers static values,
fields of type references, strings and primitive values.

Fixes array allocation that was using the number of bytes instead of the number of
elements.

Change-Id: Id3053188c451813753683e982dde287fe68b79af

perflib/src/test/java/com/android/tools/perflib/heap/HprofParserTest.java
@@ -23,6 +23,7 @@ import junit.framework.TestCase;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.util.Collection;
 
 public class HprofParserTest extends TestCase {
 
@@ -65,5 +66,42 @@ public class HprofParserTest extends TestCase {
 
         ClassObj none = object.getSuperClassObj();
         assertNull(none);
+    }
+
+    public void testObjectConstruction() {
+        ClassObj clazz = mState.findClass("java.lang.Thread$State");
+        assertNotNull(clazz);
+
+        Object object = clazz.getStaticField(Type.OBJECT, "$VALUES").getValue();
+        assertTrue(object instanceof ArrayInstance);
+        ArrayInstance array = (ArrayInstance) object;
+        Value[] values = array.getValues();
+        assertEquals(6, values.length);
+
+        Collection<Instance> instances = clazz.getInstances();
+        for (Value value : values) {
+            assertTrue(value.getValue() instanceof Instance);
+            assertTrue(instances.contains(value.getValue()));
+        }
+
+        Object blocked = clazz.getStaticField(Type.OBJECT, "NEW").getValue();
+        assertTrue(blocked instanceof ClassInstance);
+        ClassInstance instance = (ClassInstance) blocked;
+        assertSame(clazz, instance.getClassObj());
+
+        Object name = instance.getField(Type.OBJECT, "name").getValue();
+        assertTrue(name instanceof ClassInstance);
+        ClassInstance string = (ClassInstance) name;
+        assertEquals("java.lang.String", string.getClassObj().getClassName());
+        Object value = string.getField(Type.OBJECT, "value").getValue();
+        assertTrue(value instanceof ArrayInstance);
+        Value[] data = ((ArrayInstance) value).getValues();
+        assertEquals(3, data.length);
+        assertEquals('N', data[0].getValue());
+        assertEquals('E', data[1].getValue());
+        assertEquals('W', data[2].getValue());
+
+        Object ordinal = instance.getField(Type.INT, "ordinal").getValue();
+        assertEquals(0, ordinal);
     }
 }

Iandmee/base_tools

perflib/src/test/java/com/android/tools/perflib/heap/HprofParserTest.java
@@ -23,6 +23,7 @@ import junit.framework.TestCase;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.util.Collection;
 
 public class HprofParserTest extends TestCase {
 
@@ -65,5 +66,46 @@ public class HprofParserTest extends TestCase {
 
         ClassObj none = object.getSuperClassObj();
         assertNull(none);
+    }
+
+    /**
+     * Tests the creation of an Enum class which covers static values, fields of type references,
+     * strings and primitive values.
+     */
+    public void testObjectConstruction() {
+        ClassObj clazz = mState.findClass("java.lang.Thread$State");
+        assertNotNull(clazz);
+
+        Object object = clazz.getStaticField(Type.OBJECT, "$VALUES").getValue();
+        assertTrue(object instanceof ArrayInstance);
+        ArrayInstance array = (ArrayInstance) object;
+        Value[] values = array.getValues();
+        assertEquals(6, values.length);
+
+        Collection<Instance> instances = clazz.getInstances();
+        for (Value value : values) {
+            assertTrue(value.getValue() instanceof Instance);
+            assertTrue(instances.contains(value.getValue()));
+        }
+
+        Object enumValue = clazz.getStaticField(Type.OBJECT, "NEW").getValue();
+        assertTrue(enumValue instanceof ClassInstance);
+        ClassInstance instance = (ClassInstance) enumValue;
+        assertSame(clazz, instance.getClassObj());
+
+        Object name = instance.getField(Type.OBJECT, "name").getValue();
+        assertTrue(name instanceof ClassInstance);
+        ClassInstance string = (ClassInstance) name;
+        assertEquals("java.lang.String", string.getClassObj().getClassName());
+        Object value = string.getField(Type.OBJECT, "value").getValue();
+        assertTrue(value instanceof ArrayInstance);
+        Value[] data = ((ArrayInstance) value).getValues();
+        assertEquals(3, data.length);
+        assertEquals('N', data[0].getValue());
+        assertEquals('E', data[1].getValue());
+        assertEquals('W', data[2].getValue());
+
+        Object ordinal = instance.getField(Type.INT, "ordinal").getValue();
+        assertEquals(0, ordinal);
     }
 }