tadfisher/platform_tools_base

Added support for specifying maxSdkVersion on build flavors.
Only minSdkVersion and targetSdkVersion were supported up to now.
There is no implicit value set for maxSdkVersion, it must be
specified in the build.gradle.
Added unit test and build test.

Change-Id: Ibf4053ba8887cb0a6291cfc845b26123ed39b4a0

build-system/builder-model/src/main/java/com/android/builder/model/ProductFlavor.java
@@ -86,6 +86,14 @@ public interface ProductFlavor extends BaseConfig {
     ApiVersion getTargetSdkVersion();
 
     /**
+     * Returns the maxSdkVersion. This is only the value set on this produce flavor.
+     *
+     * @return the maxSdkVersion, or null if not specified
+     */
+    @Nullable
+    ApiVersion getMaxSdkVersion();
+
+    /**
      * Returns the renderscript target api. This is only the value set on this product flavor.
      * TODO: make final renderscript target api available through the model
      *

build-system/builder/src/main/java/com/android/builder/core/DefaultProductFlavor.java
@@ -45,6 +45,7 @@ public class DefaultProductFlavor extends BaseConfigImpl implements ProductFlavo
     private final String mName;
     private ApiVersion mMinSdkVersion = null;
     private ApiVersion mTargetSdkVersion = null;
+    private ApiVersion mMaxSdkVersion = null;
     private int mRenderscriptTargetApi = -1;
     private Boolean mRenderscriptSupportMode;
     private Boolean mRenderscriptNdkMode;
@@ -152,6 +153,17 @@ public class DefaultProductFlavor extends BaseConfigImpl implements ProductFlavo
     @Override
     public ApiVersion getTargetSdkVersion() {
         return mTargetSdkVersion;
+    }
+
+    @NonNull
+    public ProductFlavor setMaxSdkVersion(ApiVersion maxSdkVersion) {
+        mMaxSdkVersion = maxSdkVersion;
+        return this;
+    }
+
+    @Override
+    public ApiVersion getMaxSdkVersion() {
+        return mMaxSdkVersion;
     }
 
     @Override
@@ -304,6 +316,8 @@ public class DefaultProductFlavor extends BaseConfigImpl implements ProductFlavo
                 mMinSdkVersion != null ? mMinSdkVersion : base.mMinSdkVersion;
         flavor.mTargetSdkVersion =
                 mTargetSdkVersion != null ? mTargetSdkVersion : base.mTargetSdkVersion;
+        flavor.mMaxSdkVersion =
+                mMaxSdkVersion != null ? mMaxSdkVersion : base.mMaxSdkVersion;
         flavor.mRenderscriptTargetApi = chooseInt(mRenderscriptTargetApi,
                 base.mRenderscriptTargetApi);
         flavor.mRenderscriptSupportMode = chooseBoolean(mRenderscriptSupportMode,

build-system/gradle/src/build-test/groovy/com/android/build/gradle/ManualBuildTest.java
@@ -480,6 +480,60 @@ public class ManualBuildTest extends BuildTest {
                 String>singletonMap("META-INF/CERT.RSA", null));
     }
 
+    public void testMaxSdkVersion() throws Exception {
+        File project = new File(testDir, "maxSdkVersion");
+
+        runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_TEST_VERSION,
+                project,
+                Collections.<String>emptyList(),
+                "clean", "assembleDebug");
+        checkSdkVersion(
+                new File(project, "build/" + FD_OUTPUTS + "/apk/maxSdkVersion-f1-debug.apk"), "21");
+        checkSdkVersion(
+                new File(project, "build/" + FD_OUTPUTS + "/apk/maxSdkVersion-f2-debug.apk"), "19");
+    }
+
+    private void checkSdkVersion(File testApk, String version)
+            throws InterruptedException, LoggedErrorException, IOException {
+
+        File aapt = new File(sdkDir, "build-tools/20.0.0/aapt");
+
+        assertTrue("Test requires build-tools 20.0.0", aapt.isFile());
+
+        String[] command = new String[4];
+        command[0] = aapt.getPath();
+        command[1] = "dump";
+        command[2] = "badging";
+        command[3] = testApk.getPath();
+
+        CommandLineRunner commandLineRunner = new CommandLineRunner(new StdLogger(StdLogger.Level.ERROR));
+
+        final List<String> aaptOutput = Lists.newArrayList();
+
+        commandLineRunner.runCmdLine(command, new CommandLineRunner.CommandLineOutput() {
+            @Override
+            public void out(@Nullable String line) {
+                if (line != null) {
+                    aaptOutput.add(line);
+                }
+            }
+            @Override
+            public void err(@Nullable String line) {
+                super.err(line);
+
+            }
+        }, null /*env vars*/);
+
+        System.out.println("Beginning dump");
+        for (String line : aaptOutput) {
+            if (line.equals("maxSdkVersion:'" + version + "'")) {
+                return;
+            }
+        }
+        fail("Could not find uses-sdk:maxSdkVersion set to " + version + " in apk dump");
+    }
+
+
 
     private static void checkImageColor(File folder, String fileName, int expectedColor)
             throws IOException {

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/ProductFlavorImpl.java
@@ -43,6 +43,7 @@ class ProductFlavorImpl implements ProductFlavor, Serializable {
     private String name = null;
     private ApiVersion mMinSdkVersion = null;
     private ApiVersion mTargetSdkVersion = null;
+    private ApiVersion mMaxSdkVersion = null;
     private int mRenderscriptTargetApi = -1;
     private boolean mRenderscriptSupportMode = false;
     private boolean mRenderscriptNdkMode = false;
@@ -60,16 +61,20 @@ class ProductFlavorImpl implements ProductFlavor, Serializable {
     static ProductFlavorImpl cloneFlavor(
             @NonNull ProductFlavor productFlavor,
             @Nullable ApiVersion minSdkVersionOverride,
-            @Nullable ApiVersion targetSdkVersionOverride) {
+            @Nullable ApiVersion targetSdkVersionOverride,
+            @Nullable ApiVersion maxSdkVersionOverride) {
         ProductFlavorImpl clonedFlavor = new ProductFlavorImpl();
         clonedFlavor.name = productFlavor.getName();
 
-        clonedFlavor.mMinSdkVersion = minSdkVersionOverride != null ?
-                minSdkVersionOverride :
-                ApiVersionImpl.clone(productFlavor.getMinSdkVersion());
-        clonedFlavor.mTargetSdkVersion = targetSdkVersionOverride != null ?
-                targetSdkVersionOverride :
-                ApiVersionImpl.clone(productFlavor.getTargetSdkVersion());
+        clonedFlavor.mMinSdkVersion = minSdkVersionOverride != null
+                ? minSdkVersionOverride
+                : ApiVersionImpl.clone(productFlavor.getMinSdkVersion());
+        clonedFlavor.mTargetSdkVersion = targetSdkVersionOverride != null
+                ? targetSdkVersionOverride
+                : ApiVersionImpl.clone(productFlavor.getTargetSdkVersion());
+        clonedFlavor.mMaxSdkVersion = maxSdkVersionOverride != null
+                ? maxSdkVersionOverride
+                : ApiVersionImpl.clone(productFlavor.getMaxSdkVersion());
         clonedFlavor.mRenderscriptTargetApi = productFlavor.getRenderscriptTargetApi();
         clonedFlavor.mRenderscriptSupportMode = productFlavor.getRenderscriptSupportMode();
         clonedFlavor.mRenderscriptNdkMode = productFlavor.getRenderscriptNdkMode();
@@ -130,6 +135,10 @@ class ProductFlavorImpl implements ProductFlavor, Serializable {
     public ApiVersion getTargetSdkVersion() {
         return mTargetSdkVersion;
     }
+
+    @Override
+    @Nullable
+    public ApiVersion getMaxSdkVersion() { return mMaxSdkVersion; }
 
     @Override
     public int getRenderscriptTargetApi() {

build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/MergeManifests.groovy
@@ -64,6 +64,9 @@ public class MergeManifests extends ManifestProcessorTask {
     @Input @Optional
     String targetSdkVersion
 
+    @Input @Optional
+    String maxSdkVersion
+
     /**
      * Return a serializable version of our map of key value pairs for placeholder substitution.
      * This serialized form is only used by gradle to compare past and present tasks to determine
@@ -109,6 +112,7 @@ public class MergeManifests extends ManifestProcessorTask {
                 getVersionName(),
                 getMinSdkVersion(),
                 getTargetSdkVersion(),
+                getMaxSdkVersion(),
                 getManifestOutputFile().absolutePath,
                 ManifestMerger2.MergeType.APPLICATION,
                 variantConfiguration.getManifestPlaceholders())

build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/ProcessManifest.groovy
@@ -35,6 +35,9 @@ class ProcessManifest extends ManifestProcessorTask {
     @Input @Optional
     String targetSdkVersion
 
+    @Input @Optional
+    String maxSdkVersion
+
     VariantConfiguration variantConfiguration
 
     @InputFile
@@ -84,6 +87,7 @@ class ProcessManifest extends ManifestProcessorTask {
                 getVersionName(),
                 getMinSdkVersion(),
                 getTargetSdkVersion(),
+                getMaxSdkVersion(),
                 getManifestOutputFile().absolutePath,
                 ManifestMerger2.MergeType.LIBRARY,
                 variantConfiguration.getManifestPlaceholders())

Iandmee/base_tools

build-system/builder-model/src/main/java/com/android/builder/model/ProductFlavor.java
@@ -86,6 +86,14 @@ public interface ProductFlavor extends BaseConfig {
     ApiVersion getTargetSdkVersion();
 
     /**
+     * Returns the maxSdkVersion. This is only the value set on this produce flavor.
+     *
+     * @return the maxSdkVersion, or null if not specified
+     */
+    @Nullable
+    Integer getMaxSdkVersion();
+
+    /**
      * Returns the renderscript target api. This is only the value set on this product flavor.
      * TODO: make final renderscript target api available through the model
      *

build-system/builder/src/main/java/com/android/builder/core/DefaultProductFlavor.java
@@ -45,6 +45,7 @@ public class DefaultProductFlavor extends BaseConfigImpl implements ProductFlavo
     private final String mName;
     private ApiVersion mMinSdkVersion = null;
     private ApiVersion mTargetSdkVersion = null;
+    private Integer mMaxSdkVersion = null;
     private int mRenderscriptTargetApi = -1;
     private Boolean mRenderscriptSupportMode;
     private Boolean mRenderscriptNdkMode;
@@ -152,6 +153,17 @@ public class DefaultProductFlavor extends BaseConfigImpl implements ProductFlavo
     @Override
     public ApiVersion getTargetSdkVersion() {
         return mTargetSdkVersion;
+    }
+
+    @NonNull
+    public ProductFlavor setMaxSdkVersion(Integer maxSdkVersion) {
+        mMaxSdkVersion = maxSdkVersion;
+        return this;
+    }
+
+    @Override
+    public Integer getMaxSdkVersion() {
+        return mMaxSdkVersion;
     }
 
     @Override
@@ -304,6 +316,8 @@ public class DefaultProductFlavor extends BaseConfigImpl implements ProductFlavo
                 mMinSdkVersion != null ? mMinSdkVersion : base.mMinSdkVersion;
         flavor.mTargetSdkVersion =
                 mTargetSdkVersion != null ? mTargetSdkVersion : base.mTargetSdkVersion;
+        flavor.mMaxSdkVersion =
+                mMaxSdkVersion != null ? mMaxSdkVersion : base.mMaxSdkVersion;
         flavor.mRenderscriptTargetApi = chooseInt(mRenderscriptTargetApi,
                 base.mRenderscriptTargetApi);
         flavor.mRenderscriptSupportMode = chooseBoolean(mRenderscriptSupportMode,

build-system/gradle/src/build-test/groovy/com/android/build/gradle/ManualBuildTest.java
@@ -480,6 +480,60 @@ public class ManualBuildTest extends BuildTest {
                 String>singletonMap("META-INF/CERT.RSA", null));
     }
 
+    public void testMaxSdkVersion() throws Exception {
+        File project = new File(testDir, "maxSdkVersion");
+
+        runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_TEST_VERSION,
+                project,
+                Collections.<String>emptyList(),
+                "clean", "assembleDebug");
+        checkMaxSdkVersion(
+                new File(project, "build/" + FD_OUTPUTS + "/apk/maxSdkVersion-f1-debug.apk"), "21");
+        checkMaxSdkVersion(
+                new File(project, "build/" + FD_OUTPUTS + "/apk/maxSdkVersion-f2-debug.apk"), "19");
+    }
+
+    private void checkMaxSdkVersion(File testApk, String version)
+            throws InterruptedException, LoggedErrorException, IOException {
+
+        File aapt = new File(sdkDir, "build-tools/20.0.0/aapt");
+
+        assertTrue("Test requires build-tools 20.0.0", aapt.isFile());
+
+        String[] command = new String[4];
+        command[0] = aapt.getPath();
+        command[1] = "dump";
+        command[2] = "badging";
+        command[3] = testApk.getPath();
+
+        CommandLineRunner commandLineRunner = new CommandLineRunner(new StdLogger(StdLogger.Level.ERROR));
+
+        final List<String> aaptOutput = Lists.newArrayList();
+
+        commandLineRunner.runCmdLine(command, new CommandLineRunner.CommandLineOutput() {
+            @Override
+            public void out(@Nullable String line) {
+                if (line != null) {
+                    aaptOutput.add(line);
+                }
+            }
+            @Override
+            public void err(@Nullable String line) {
+                super.err(line);
+
+            }
+        }, null /*env vars*/);
+
+        System.out.println("Beginning dump");
+        for (String line : aaptOutput) {
+            if (line.equals("maxSdkVersion:'" + version + "'")) {
+                return;
+            }
+        }
+        fail("Could not find uses-sdk:maxSdkVersion set to " + version + " in apk dump");
+    }
+
+
 
     private static void checkImageColor(File folder, String fileName, int expectedColor)
             throws IOException {

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/ProductFlavorImpl.java
@@ -43,6 +43,7 @@ class ProductFlavorImpl implements ProductFlavor, Serializable {
     private String name = null;
     private ApiVersion mMinSdkVersion = null;
     private ApiVersion mTargetSdkVersion = null;
+    private Integer mMaxSdkVersion = null;
     private int mRenderscriptTargetApi = -1;
     private boolean mRenderscriptSupportMode = false;
     private boolean mRenderscriptNdkMode = false;
@@ -64,12 +65,15 @@ class ProductFlavorImpl implements ProductFlavor, Serializable {
         ProductFlavorImpl clonedFlavor = new ProductFlavorImpl();
         clonedFlavor.name = productFlavor.getName();
 
-        clonedFlavor.mMinSdkVersion = minSdkVersionOverride != null ?
-                minSdkVersionOverride :
-                ApiVersionImpl.clone(productFlavor.getMinSdkVersion());
-        clonedFlavor.mTargetSdkVersion = targetSdkVersionOverride != null ?
-                targetSdkVersionOverride :
-                ApiVersionImpl.clone(productFlavor.getTargetSdkVersion());
+        clonedFlavor.mMinSdkVersion = minSdkVersionOverride != null
+                ? minSdkVersionOverride
+                : ApiVersionImpl.clone(productFlavor.getMinSdkVersion());
+        clonedFlavor.mTargetSdkVersion = targetSdkVersionOverride != null
+                ? targetSdkVersionOverride
+                : ApiVersionImpl.clone(productFlavor.getTargetSdkVersion());
+        clonedFlavor.mMaxSdkVersion = targetSdkVersionOverride != null
+                ? null /* we remove the maxSdkVersion when dealing with a preview release */
+                : productFlavor.getMaxSdkVersion();
         clonedFlavor.mRenderscriptTargetApi = productFlavor.getRenderscriptTargetApi();
         clonedFlavor.mRenderscriptSupportMode = productFlavor.getRenderscriptSupportMode();
         clonedFlavor.mRenderscriptNdkMode = productFlavor.getRenderscriptNdkMode();
@@ -130,6 +134,10 @@ class ProductFlavorImpl implements ProductFlavor, Serializable {
     public ApiVersion getTargetSdkVersion() {
         return mTargetSdkVersion;
     }
+
+    @Override
+    @Nullable
+    public Integer getMaxSdkVersion() { return mMaxSdkVersion; }
 
     @Override
     public int getRenderscriptTargetApi() {

build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/MergeManifests.groovy
@@ -64,6 +64,9 @@ public class MergeManifests extends ManifestProcessorTask {
     @Input @Optional
     String targetSdkVersion
 
+    @Input @Optional
+    Integer maxSdkVersion
+
     /**
      * Return a serializable version of our map of key value pairs for placeholder substitution.
      * This serialized form is only used by gradle to compare past and present tasks to determine
@@ -109,6 +112,7 @@ public class MergeManifests extends ManifestProcessorTask {
                 getVersionName(),
                 getMinSdkVersion(),
                 getTargetSdkVersion(),
+                getMaxSdkVersion(),
                 getManifestOutputFile().absolutePath,
                 ManifestMerger2.MergeType.APPLICATION,
                 variantConfiguration.getManifestPlaceholders())

build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/ProcessManifest.groovy
@@ -35,6 +35,9 @@ class ProcessManifest extends ManifestProcessorTask {
     @Input @Optional
     String targetSdkVersion
 
+    @Input @Optional
+    Integer maxSdkVersion
+
     VariantConfiguration variantConfiguration
 
     @InputFile
@@ -84,6 +87,7 @@ class ProcessManifest extends ManifestProcessorTask {
                 getVersionName(),
                 getMinSdkVersion(),
                 getTargetSdkVersion(),
+                getMaxSdkVersion(),
                 getManifestOutputFile().absolutePath,
                 ManifestMerger2.MergeType.LIBRARY,
                 variantConfiguration.getManifestPlaceholders())