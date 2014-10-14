tadfisher/platform_tools_base

Add ItemResourceValue
The new class represents an item in the style resource. Earlier,
ResourceValue was used along with a boolean to represent the attribute
namespace.

Change-Id: I03344f7561952260d8155d6a39119b4b54d38285

lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/LintUtils.java
@@ -34,6 +34,7 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.model.AndroidProject;
 import com.android.builder.model.ApiVersion;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.ide.common.res2.AbstractResourceRepository;
@@ -860,7 +861,7 @@ public class LintUtils {
                     ResourceValue rv = item.getResourceValue(false);
                     if (rv instanceof StyleResourceValue) {
                         StyleResourceValue srv = (StyleResourceValue) rv;
-                        ResourceValue value = srv.findValue(attribute, isFrameworkAttribute);
+                        ItemResourceValue value = srv.findVal(attribute, isFrameworkAttribute);
                         if (value != null) {
                             if (result == null) {
                                 result = Lists.newArrayList();

sdk-common/src/main/java/com/android/ide/common/res2/ResourceItem.java
@@ -39,6 +39,7 @@ import com.android.ide.common.rendering.api.ArrayResourceValue;
 import com.android.ide.common.rendering.api.AttrResourceValue;
 import com.android.ide.common.rendering.api.DeclareStyleableResourceValue;
 import com.android.ide.common.rendering.api.DensityBasedResourceValue;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.PluralsResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
@@ -283,11 +284,8 @@ public class ResourceItem extends DataItem<ResourceFile>
 
         ResourceItem that = (ResourceItem) o;
 
-        if (mType != that.mType) {
-            return false;
-        }
+        return mType == that.mType;
 
-        return true;
     }
 
     @Override
@@ -391,11 +389,11 @@ public class ResourceItem extends DataItem<ResourceFile>
                         isFrameworkAttr = true;
                     }
 
-                    ResourceValue resValue = new ResourceValue(null, name,
+                    ItemResourceValue resValue = new ItemResourceValue(name, isFrameworkAttr,
                             styleValue.isFramework());
                     String text = getTextNode(child.getChildNodes());
                     resValue.setValue(ValueXmlHelper.unescapeResourceString(text, false, true));
-                    styleValue.addValue(resValue, isFrameworkAttr);
+                    styleValue.addValue(resValue);
                 }
             }
         }

sdk-common/src/main/java/com/android/ide/common/resources/ResourceResolver.java
@@ -253,7 +253,7 @@ public class ResourceResolver extends RenderResources {
 
     private ResourceValue findItemInStyle(StyleResourceValue style, String itemName,
                                           boolean isFrameworkAttr, int depth) {
-        ResourceValue item = style.findValue(itemName, isFrameworkAttr);
+        ResourceValue item = style.findVal(itemName, isFrameworkAttr);
 
         // if we didn't find it, we look in the parent style (if applicable)
         //noinspection VariableNotUsedInsideIf

sdk-common/src/main/java/com/android/ide/common/resources/ValueResourceParser.java
@@ -18,6 +18,7 @@ package com.android.ide.common.resources;
 
 import com.android.ide.common.rendering.api.AttrResourceValue;
 import com.android.ide.common.rendering.api.DeclareStyleableResourceValue;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.ide.common.res2.ValueXmlHelper;
@@ -140,8 +141,8 @@ public final class ValueResourceParser extends DefaultHandler {
                             isFrameworkAttr = true;
                         }
 
-                        mCurrentValue = new ResourceValue(null, name, mIsFramework);
-                        mCurrentStyle.addValue(mCurrentValue, isFrameworkAttr);
+                        mCurrentValue = new ItemResourceValue(name, isFrameworkAttr, mIsFramework);
+                        mCurrentStyle.addValue((ItemResourceValue) mCurrentValue);
                     } else if (mCurrentDeclareStyleable != null) {
                         // is the attribute in the android namespace?
                         boolean isFramework = mIsFramework;

sdk-common/src/test/java/com/android/ide/common/res2/ResourceRepositoryTest.java
@@ -17,6 +17,7 @@
 package com.android.ide.common.res2;
 
 import com.android.ide.common.rendering.api.AttrResourceValue;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.resources.ResourceFolderType;
@@ -188,23 +189,23 @@ public class ResourceRepositoryTest extends BaseTestCase {
 
         assertEquals("@android:style/Holo.Light", styleResourceValue.getParentStyle());
 
-        ResourceValue styleValue = styleResourceValue.findValue("singleLine", true /*framework*/);
+        ItemResourceValue styleValue = styleResourceValue.findVal("singleLine", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("true", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("textAppearance", true /*framework*/);
+        styleValue = styleResourceValue.findVal("textAppearance", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("@style/TextAppearance.WindowTitle", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("shadowColor", true /*framework*/);
+        styleValue = styleResourceValue.findVal("shadowColor", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("#BB000000", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("shadowRadius", true /*framework*/);
+        styleValue = styleResourceValue.findVal("shadowRadius", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("2.75", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("foo", false /*framework*/);
+        styleValue = styleResourceValue.findVal("foo", false /*framework*/);
         assertNotNull(styleValue);
         assertEquals("foo", styleValue.getValue());
     }
@@ -519,8 +520,6 @@ public class ResourceRepositoryTest extends BaseTestCase {
      *
      * Each set is [ setName, folder1, folder2, ...]
      *
-     * @param data
-     * @return
      */
     private static ResourceMerger createMerger(String[][] data) {
         ResourceMerger merger = new ResourceMerger();
@@ -702,16 +701,15 @@ public class ResourceRepositoryTest extends BaseTestCase {
                                 sb.append("  parentStyle=").append(srv.getParentStyle())
                                         .append("\n");
                                 for (String name : srv.getNames()) {
-                                    ResourceValue value1 = srv.findValue(name, false);
-                                    ResourceValue value2 = srv.findValue(name, true);
+                                    ItemResourceValue value1 = srv.findVal(name, false);
+                                    ItemResourceValue value2 = srv.findVal(name, true);
                                     if (value1 != null) {
                                         Boolean framework = false;
-                                        ResourceValue v = value1;
                                         sb.append("    ");
                                         sb.append(name).append(" ").append(framework).append(" ");
                                         sb.append(" = ");
                                         sb.append('"');
-                                        String strValue = v.getValue();
+                                        String strValue = value1.getValue();
                                         if (strValue != null) {
                                             sb.append(strValue.replace("\n", "\\n"));
                                         } else {
@@ -721,12 +719,11 @@ public class ResourceRepositoryTest extends BaseTestCase {
                                     }
                                     if (value2 != null) {
                                         Boolean framework = true;
-                                        ResourceValue v = value2;
                                         sb.append("    ");
                                         sb.append(name).append(" ").append(framework).append(" ");
                                         sb.append(" = ");
                                         sb.append('"');
-                                        String strValue = v.getValue();
+                                        String strValue = value2.getValue();
                                         if (strValue != null) {
                                             sb.append(strValue.replace("\n", "\\n"));
                                         } else {

sdk-common/src/test/java/com/android/ide/common/resources/ResourceResolverTest.java
@@ -187,7 +187,7 @@ public class ResourceResolverTest extends TestCase {
         //    check XML escaping in value resources
         StyleResourceValue randomStyle = (StyleResourceValue) resolver.findResValue(
                 "@style/RandomStyle", false);
-        assertEquals("\u00a9 Copyright", randomStyle.findValue("text", true).getValue());
+        assertEquals("\u00a9 Copyright", randomStyle.findVal("text", true).getValue());
         assertTrue(resolver.isTheme(resolver.findResValue("@style/MyTheme.Dotted2", false), null));
         assertFalse(resolver.isTheme(resolver.findResValue("@style/MyTheme.Dotted1", false),
                 null));

Iandmee/base_tools

lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/LintUtils.java
@@ -34,6 +34,7 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.model.AndroidProject;
 import com.android.builder.model.ApiVersion;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.ide.common.res2.AbstractResourceRepository;
@@ -860,7 +861,7 @@ public class LintUtils {
                     ResourceValue rv = item.getResourceValue(false);
                     if (rv instanceof StyleResourceValue) {
                         StyleResourceValue srv = (StyleResourceValue) rv;
-                        ResourceValue value = srv.findValue(attribute, isFrameworkAttribute);
+                        ItemResourceValue value = srv.getItem(attribute, isFrameworkAttribute);
                         if (value != null) {
                             if (result == null) {
                                 result = Lists.newArrayList();

sdk-common/src/main/java/com/android/ide/common/res2/ResourceItem.java
@@ -39,6 +39,7 @@ import com.android.ide.common.rendering.api.ArrayResourceValue;
 import com.android.ide.common.rendering.api.AttrResourceValue;
 import com.android.ide.common.rendering.api.DeclareStyleableResourceValue;
 import com.android.ide.common.rendering.api.DensityBasedResourceValue;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.PluralsResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
@@ -283,11 +284,8 @@ public class ResourceItem extends DataItem<ResourceFile>
 
         ResourceItem that = (ResourceItem) o;
 
-        if (mType != that.mType) {
-            return false;
-        }
+        return mType == that.mType;
 
-        return true;
     }
 
     @Override
@@ -391,11 +389,11 @@ public class ResourceItem extends DataItem<ResourceFile>
                         isFrameworkAttr = true;
                     }
 
-                    ResourceValue resValue = new ResourceValue(null, name,
+                    ItemResourceValue resValue = new ItemResourceValue(name, isFrameworkAttr,
                             styleValue.isFramework());
                     String text = getTextNode(child.getChildNodes());
                     resValue.setValue(ValueXmlHelper.unescapeResourceString(text, false, true));
-                    styleValue.addValue(resValue, isFrameworkAttr);
+                    styleValue.addItem(resValue);
                 }
             }
         }

sdk-common/src/main/java/com/android/ide/common/resources/ResourceResolver.java
@@ -253,7 +253,7 @@ public class ResourceResolver extends RenderResources {
 
     private ResourceValue findItemInStyle(StyleResourceValue style, String itemName,
                                           boolean isFrameworkAttr, int depth) {
-        ResourceValue item = style.findValue(itemName, isFrameworkAttr);
+        ResourceValue item = style.getItem(itemName, isFrameworkAttr);
 
         // if we didn't find it, we look in the parent style (if applicable)
         //noinspection VariableNotUsedInsideIf

sdk-common/src/main/java/com/android/ide/common/resources/ValueResourceParser.java
@@ -18,6 +18,7 @@ package com.android.ide.common.resources;
 
 import com.android.ide.common.rendering.api.AttrResourceValue;
 import com.android.ide.common.rendering.api.DeclareStyleableResourceValue;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.ide.common.res2.ValueXmlHelper;
@@ -140,8 +141,8 @@ public final class ValueResourceParser extends DefaultHandler {
                             isFrameworkAttr = true;
                         }
 
-                        mCurrentValue = new ResourceValue(null, name, mIsFramework);
-                        mCurrentStyle.addValue(mCurrentValue, isFrameworkAttr);
+                        mCurrentValue = new ItemResourceValue(name, isFrameworkAttr, mIsFramework);
+                        mCurrentStyle.addItem((ItemResourceValue)mCurrentValue);
                     } else if (mCurrentDeclareStyleable != null) {
                         // is the attribute in the android namespace?
                         boolean isFramework = mIsFramework;

sdk-common/src/test/java/com/android/ide/common/res2/ResourceRepositoryTest.java
@@ -17,6 +17,7 @@
 package com.android.ide.common.res2;
 
 import com.android.ide.common.rendering.api.AttrResourceValue;
+import com.android.ide.common.rendering.api.ItemResourceValue;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.StyleResourceValue;
 import com.android.resources.ResourceFolderType;
@@ -188,23 +189,23 @@ public class ResourceRepositoryTest extends BaseTestCase {
 
         assertEquals("@android:style/Holo.Light", styleResourceValue.getParentStyle());
 
-        ResourceValue styleValue = styleResourceValue.findValue("singleLine", true /*framework*/);
+        ItemResourceValue styleValue = styleResourceValue.getItem("singleLine", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("true", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("textAppearance", true /*framework*/);
+        styleValue = styleResourceValue.getItem("textAppearance", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("@style/TextAppearance.WindowTitle", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("shadowColor", true /*framework*/);
+        styleValue = styleResourceValue.getItem("shadowColor", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("#BB000000", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("shadowRadius", true /*framework*/);
+        styleValue = styleResourceValue.getItem("shadowRadius", true /*framework*/);
         assertNotNull(styleValue);
         assertEquals("2.75", styleValue.getValue());
 
-        styleValue = styleResourceValue.findValue("foo", false /*framework*/);
+        styleValue = styleResourceValue.getItem("foo", false /*framework*/);
         assertNotNull(styleValue);
         assertEquals("foo", styleValue.getValue());
     }
@@ -519,8 +520,6 @@ public class ResourceRepositoryTest extends BaseTestCase {
      *
      * Each set is [ setName, folder1, folder2, ...]
      *
-     * @param data
-     * @return
      */
     private static ResourceMerger createMerger(String[][] data) {
         ResourceMerger merger = new ResourceMerger();
@@ -702,16 +701,15 @@ public class ResourceRepositoryTest extends BaseTestCase {
                                 sb.append("  parentStyle=").append(srv.getParentStyle())
                                         .append("\n");
                                 for (String name : srv.getNames()) {
-                                    ResourceValue value1 = srv.findValue(name, false);
-                                    ResourceValue value2 = srv.findValue(name, true);
+                                    ItemResourceValue value1 = srv.getItem(name, false);
+                                    ItemResourceValue value2 = srv.getItem(name, true);
                                     if (value1 != null) {
                                         Boolean framework = false;
-                                        ResourceValue v = value1;
                                         sb.append("    ");
                                         sb.append(name).append(" ").append(framework).append(" ");
                                         sb.append(" = ");
                                         sb.append('"');
-                                        String strValue = v.getValue();
+                                        String strValue = value1.getValue();
                                         if (strValue != null) {
                                             sb.append(strValue.replace("\n", "\\n"));
                                         } else {
@@ -721,12 +719,11 @@ public class ResourceRepositoryTest extends BaseTestCase {
                                     }
                                     if (value2 != null) {
                                         Boolean framework = true;
-                                        ResourceValue v = value2;
                                         sb.append("    ");
                                         sb.append(name).append(" ").append(framework).append(" ");
                                         sb.append(" = ");
                                         sb.append('"');
-                                        String strValue = v.getValue();
+                                        String strValue = value2.getValue();
                                         if (strValue != null) {
                                             sb.append(strValue.replace("\n", "\\n"));
                                         } else {

sdk-common/src/test/java/com/android/ide/common/resources/ResourceResolverTest.java
@@ -187,7 +187,7 @@ public class ResourceResolverTest extends TestCase {
         //    check XML escaping in value resources
         StyleResourceValue randomStyle = (StyleResourceValue) resolver.findResValue(
                 "@style/RandomStyle", false);
-        assertEquals("\u00a9 Copyright", randomStyle.findValue("text", true).getValue());
+        assertEquals("\u00a9 Copyright", randomStyle.getItem("text", true).getValue());
         assertTrue(resolver.isTheme(resolver.findResValue("@style/MyTheme.Dotted2", false), null));
         assertFalse(resolver.isTheme(resolver.findResValue("@style/MyTheme.Dotted1", false),
                 null));