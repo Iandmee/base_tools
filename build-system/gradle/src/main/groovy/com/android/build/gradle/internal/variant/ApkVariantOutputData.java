tadfisher/platform_tools_base

added preliminary support for selecting split APKs
Added support to match generated split APKs against deployment
device.
Added a flag to enable/disable pure split support (disabled by default).

Change-Id: I4952142dbae8915d767f1137bb872c90b1078174

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/ApkVariantOutputData.java
@@ -67,8 +67,12 @@ public class ApkVariantOutputData extends BaseVariantOutputData {
     @Override
     public ImmutableList<ApkOutput> getOutputFiles() {
         ImmutableList.Builder<ApkOutput> outputs = ImmutableList.builder();
-        if (packageSplitResourcesTask != null) {
-            outputs.addAll(packageSplitResourcesTask.getOutputFiles());
+        if (splitZipAlign != null) {
+            outputs.addAll(splitZipAlign.getOutputFiles());
+        } else {
+            if (packageSplitResourcesTask != null) {
+                outputs.addAll(packageSplitResourcesTask.getOutputFiles());
+            }
         }
         outputs.add(new ApkOutput.MainApkOutput(getOutputFile()));
         return outputs.build();

Iandmee/base_tools

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/ApkVariantOutputData.java
@@ -20,6 +20,7 @@ import com.android.annotations.NonNull;
 import com.android.build.FilterData;
 import com.android.build.OutputFile;
 import com.android.build.gradle.api.ApkOutputFile;
+import com.android.build.gradle.api.MainApkOutputFile;
 import com.android.build.gradle.tasks.PackageApplication;
 import com.android.build.gradle.tasks.SplitZipAlign;
 import com.android.build.gradle.tasks.ZipAlign;
@@ -70,8 +71,12 @@ public class ApkVariantOutputData extends BaseVariantOutputData {
     public ImmutableList<ApkOutputFile> getOutputs() {
         ImmutableList.Builder<ApkOutputFile> outputs = ImmutableList.builder();
         outputs.add(getMainOutputFile());
-        if (packageSplitResourcesTask != null) {
-            outputs.addAll(packageSplitResourcesTask.getOutputSplitFiles());
+        if (splitZipAlign != null) {
+            outputs.addAll(splitZipAlign.getOutputSplitFiles());
+        } else {
+            if (packageSplitResourcesTask != null) {
+                outputs.addAll(packageSplitResourcesTask.getOutputSplitFiles());
+            }
         }
         return outputs.build();
     }