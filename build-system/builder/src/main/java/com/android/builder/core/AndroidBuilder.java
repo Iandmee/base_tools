tadfisher/platform_tools_base

Full multi-dex support with Jack.
Supports both native and legacy mode.

- expects $BUILDTOOLS/multidex/android-support-multidex.jar
- support jar gets added to the dependencies automatically, only
  if minSdkVersion < 21

Change-Id: I88df37bc9e9e8147a4b0dce3a95e102eaa7e27b8

build-system/builder/src/main/java/com/android/builder/core/AndroidBuilder.java
@@ -19,6 +19,7 @@ package com.android.builder.core;
 import static com.android.SdkConstants.DOT_DEX;
 import static com.android.SdkConstants.DOT_XML;
 import static com.android.SdkConstants.FD_RES_XML;
+import static com.android.SdkConstants.FN_RENDERSCRIPT_V8_JAR;
 import static com.android.builder.core.BuilderConstants.ANDROID_WEAR;
 import static com.android.builder.core.BuilderConstants.ANDROID_WEAR_MICRO_APK;
 import static com.android.manifmerger.ManifestMerger2.Invoker;
@@ -329,6 +330,25 @@ public class AndroidBuilder {
     }
 
     /**
+     * Returns the jar file for the multi-dex legacy mode.
+     *
+     * This may return null if the SDK has not been loaded yet.
+     *
+     * @return the jar file, or null.
+     *
+     * @see #setTargetInfo(com.android.builder.sdk.SdkInfo, com.android.builder.sdk.TargetInfo)
+     */
+    @Nullable
+    public File getMultiDexSupportJar() {
+        if (mTargetInfo != null) {
+            return new File(mTargetInfo.getBuildTools().getLocation().getAbsolutePath(),
+                    "multidex" + File.separator + "android-support-multidex.jar");
+        }
+
+        return null;
+    }
+
+    /**
      * Returns the compile classpath for this config. If the config tests a library, this
      * will include the classpath of the tested config.
      *
@@ -364,17 +384,21 @@ public class AndroidBuilder {
      */
     @NonNull
     public Set<File> getPackagedJars(@NonNull VariantConfiguration<?,?,?> variantConfiguration) {
-        Set<File> packagedJars = variantConfiguration.getPackagedJars();
+        Set<File> packagedJars = Sets.newHashSet(variantConfiguration.getPackagedJars());
 
         if (variantConfiguration.getRenderscriptSupportMode()) {
             File renderScriptSupportJar = getRenderScriptSupportJar();
 
-            Set<File> fullJars = Sets.newHashSetWithExpectedSize(packagedJars.size() + 1);
-            fullJars.addAll(packagedJars);
             if (renderScriptSupportJar != null) {
-                fullJars.add(renderScriptSupportJar);
+                packagedJars.add(renderScriptSupportJar);
             }
-            packagedJars = fullJars;
+        }
+
+        if (variantConfiguration.isMultiDexLegacyMode()) {
+            File supportJar = getMultiDexSupportJar();
+            if (supportJar != null) {
+                packagedJars.add(supportJar);
+            }
         }
 
         return packagedJars;

build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -1546,7 +1546,7 @@ public abstract class BasePlugin {
         }
 
         // Add a task to compile the test application
-        if (variantData.getVariantConfiguration().getBuildType().getUseJack()) {
+        if (variantData.getVariantConfiguration().useJack) {
             createJackTask(variantData, testedVariantData);
         } else{
             createCompileTask(variantData, testedVariantData)
@@ -1631,7 +1631,7 @@ public abstract class BasePlugin {
             def taskName = "lintVital" + capitalizedVariantName
             Lint lintReleaseCheck = project.tasks.create(taskName, Lint)
             // TODO: Make this task depend on lintCompile too (resolve initialization order first)
-            lintReleaseCheck.dependsOn variantData.javaCompileTask
+            optionalDependsOn(lintReleaseCheck, variantData.javaCompileTask)
             lintReleaseCheck.setPlugin(this)
             lintReleaseCheck.setVariantName(variantName)
             lintReleaseCheck.setFatalOnly(true)
@@ -1907,15 +1907,6 @@ public abstract class BasePlugin {
         return testTask
     }
 
-    static boolean isMultiDex(VariantConfiguration config) {
-        ApiVersion minSdkVersion = config.minSdkVersion
-        if (minSdkVersion.apiLevel >= 21) {
-            return config.multiDex
-        }
-
-        return false
-    }
-
     /**
      * Creates the post-compilation tasks for the given Variant.
      *
@@ -1926,6 +1917,10 @@ public abstract class BasePlugin {
      */
     public void createPostCompilationTasks(@NonNull ApkVariantData variantData) {
         GradleVariantConfiguration config = variantData.variantConfiguration
+
+        if (config.isMultiDexLegacyMode()) {
+            throw new RuntimeException("Legacy mode not supported yet. Use Jack.")
+        }
 
         boolean isMinifyEnabled = config.isMinifyEnabled()
 
@@ -1945,7 +1940,7 @@ public abstract class BasePlugin {
         dexTask.dexOptions = extension.dexOptions
 
         dexTask.conventionMapping.multiDex = {
-            isMultiDex(config)
+            config.isMultiDex()
         }
 
         JacocoInstrumentTask jacocoTask = null
@@ -1978,6 +1973,7 @@ public abstract class BasePlugin {
             dexTask.conventionMapping.inputFiles = { project.files(outFile).files }
             dexTask.conventionMapping.libraries = { Collections.emptyList() }
         } else {
+
             // if required, pre-dexing task.
             PreDex preDexTask = null;
             boolean runPreDex = extension.dexOptions.preDexLibraries
@@ -1991,7 +1987,7 @@ public abstract class BasePlugin {
                 preDexTask.dexOptions = extension.dexOptions
 
                 preDexTask.conventionMapping.multiDex = {
-                    return isMultiDex(config)
+                    return config.isMultiDex()
                 }
 
                 preDexTask.conventionMapping.inputFiles = {
@@ -2032,7 +2028,7 @@ public abstract class BasePlugin {
                 // if multi-dex is enabled, then each library will just be added to the
                 // package task.
                 dexTask.conventionMapping.libraries = {
-                    if (isMultiDex(config)) {
+                    if (config.isMultiDex()) {
                         return Collections.emptyList()
                     }
 
@@ -2112,6 +2108,9 @@ public abstract class BasePlugin {
         compileTask.plugin = this
 
         compileTask.source = variantData.getJavaSources()
+
+        compileTask.multiDexEnabled = config.isMultiDex()
+        compileTask.minSdkVersion = config.minSdkVersion.apiLevel
 
         // if the tested variant is an app, add its classpath. For the libraries,
         // it's done automatically since the classpath includes the library output as a normal
@@ -2248,7 +2247,7 @@ public abstract class BasePlugin {
                 return null
             }
             packageApp.conventionMapping.dexedLibraries = {
-                if (isMultiDex(config) && variantData.preDexTask != null) {
+                if (config.isMultiDex() && variantData.preDexTask != null) {
                     return project.fileTree(variantData.preDexTask.outputFolder).files
                 }
 

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/DependenciesImpl.java
@@ -21,9 +21,11 @@ import static com.android.SdkConstants.DOT_JAR;
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.build.gradle.BasePlugin;
+import com.android.build.gradle.internal.core.GradleVariantConfiguration;
 import com.android.build.gradle.internal.dependency.LibraryDependencyImpl;
 import com.android.build.gradle.internal.dependency.VariantDependencies;
 import com.android.build.gradle.internal.variant.BaseVariantData;
+import com.android.builder.core.AndroidBuilder;
 import com.android.builder.dependency.JarDependency;
 import com.android.builder.dependency.LibraryDependency;
 import com.android.builder.model.AndroidLibrary;
@@ -112,8 +114,18 @@ public class DependenciesImpl implements Dependencies, Serializable {
                             jarDep.getResolvedCoordinates()));
         }
 
-        if (variantData.getVariantConfiguration().getRenderscriptSupportMode()) {
-            File supportJar = basePlugin.getAndroidBuilder().getRenderScriptSupportJar();
+        GradleVariantConfiguration variantConfig = variantData.getVariantConfiguration();
+        AndroidBuilder androidBuilder = basePlugin.getAndroidBuilder();
+
+        if (variantConfig.getRenderscriptSupportMode()) {
+            File supportJar = androidBuilder.getRenderScriptSupportJar();
+            if (supportJar != null) {
+                javaLibraries.add(new JavaLibraryImpl(supportJar, null, null));
+            }
+        }
+
+        if (variantConfig.isMultiDexLegacyMode()) {
+            File supportJar = androidBuilder.getMultiDexSupportJar();
             if (supportJar != null) {
                 javaLibraries.add(new JavaLibraryImpl(supportJar, null, null));
             }

Iandmee/base_tools

build-system/builder/src/main/java/com/android/builder/core/AndroidBuilder.java
@@ -329,6 +329,25 @@ public class AndroidBuilder {
     }
 
     /**
+     * Returns the jar file for the multi-dex legacy mode.
+     *
+     * This may return null if the SDK has not been loaded yet.
+     *
+     * @return the jar file, or null.
+     *
+     * @see #setTargetInfo(com.android.builder.sdk.SdkInfo, com.android.builder.sdk.TargetInfo)
+     */
+    @Nullable
+    public File getMultiDexSupportJar() {
+        if (mTargetInfo != null) {
+            return new File(mTargetInfo.getBuildTools().getLocation().getAbsolutePath(),
+                    "multidex" + File.separator + "android-support-multidex.jar");
+        }
+
+        return null;
+    }
+
+    /**
      * Returns the compile classpath for this config. If the config tests a library, this
      * will include the classpath of the tested config.
      *
@@ -364,17 +383,21 @@ public class AndroidBuilder {
      */
     @NonNull
     public Set<File> getPackagedJars(@NonNull VariantConfiguration<?,?,?> variantConfiguration) {
-        Set<File> packagedJars = variantConfiguration.getPackagedJars();
+        Set<File> packagedJars = Sets.newHashSet(variantConfiguration.getPackagedJars());
 
         if (variantConfiguration.getRenderscriptSupportMode()) {
             File renderScriptSupportJar = getRenderScriptSupportJar();
 
-            Set<File> fullJars = Sets.newHashSetWithExpectedSize(packagedJars.size() + 1);
-            fullJars.addAll(packagedJars);
             if (renderScriptSupportJar != null) {
-                fullJars.add(renderScriptSupportJar);
+                packagedJars.add(renderScriptSupportJar);
             }
-            packagedJars = fullJars;
+        }
+
+        if (variantConfiguration.isLegacyMultiDexMode()) {
+            File supportJar = getMultiDexSupportJar();
+            if (supportJar != null) {
+                packagedJars.add(supportJar);
+            }
         }
 
         return packagedJars;

build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -1546,7 +1546,7 @@ public abstract class BasePlugin {
         }
 
         // Add a task to compile the test application
-        if (variantData.getVariantConfiguration().getBuildType().getUseJack()) {
+        if (variantData.getVariantConfiguration().useJack) {
             createJackTask(variantData, testedVariantData);
         } else{
             createCompileTask(variantData, testedVariantData)
@@ -1631,7 +1631,7 @@ public abstract class BasePlugin {
             def taskName = "lintVital" + capitalizedVariantName
             Lint lintReleaseCheck = project.tasks.create(taskName, Lint)
             // TODO: Make this task depend on lintCompile too (resolve initialization order first)
-            lintReleaseCheck.dependsOn variantData.javaCompileTask
+            optionalDependsOn(lintReleaseCheck, variantData.javaCompileTask)
             lintReleaseCheck.setPlugin(this)
             lintReleaseCheck.setVariantName(variantName)
             lintReleaseCheck.setFatalOnly(true)
@@ -1907,15 +1907,6 @@ public abstract class BasePlugin {
         return testTask
     }
 
-    static boolean isMultiDex(VariantConfiguration config) {
-        ApiVersion minSdkVersion = config.minSdkVersion
-        if (minSdkVersion.apiLevel >= 21) {
-            return config.multiDex
-        }
-
-        return false
-    }
-
     /**
      * Creates the post-compilation tasks for the given Variant.
      *
@@ -1926,6 +1917,10 @@ public abstract class BasePlugin {
      */
     public void createPostCompilationTasks(@NonNull ApkVariantData variantData) {
         GradleVariantConfiguration config = variantData.variantConfiguration
+
+        if (config.isLegacyMultiDexMode()) {
+            throw new RuntimeException("Legacy mode not supported yet. Use Jack.")
+        }
 
         boolean isMinifyEnabled = config.isMinifyEnabled()
 
@@ -1945,7 +1940,7 @@ public abstract class BasePlugin {
         dexTask.dexOptions = extension.dexOptions
 
         dexTask.conventionMapping.multiDex = {
-            isMultiDex(config)
+            config.isMultiDexEnabled()
         }
 
         JacocoInstrumentTask jacocoTask = null
@@ -1978,6 +1973,7 @@ public abstract class BasePlugin {
             dexTask.conventionMapping.inputFiles = { project.files(outFile).files }
             dexTask.conventionMapping.libraries = { Collections.emptyList() }
         } else {
+
             // if required, pre-dexing task.
             PreDex preDexTask = null;
             boolean runPreDex = extension.dexOptions.preDexLibraries
@@ -1991,7 +1987,7 @@ public abstract class BasePlugin {
                 preDexTask.dexOptions = extension.dexOptions
 
                 preDexTask.conventionMapping.multiDex = {
-                    return isMultiDex(config)
+                    return config.isMultiDexEnabled()
                 }
 
                 preDexTask.conventionMapping.inputFiles = {
@@ -2032,7 +2028,7 @@ public abstract class BasePlugin {
                 // if multi-dex is enabled, then each library will just be added to the
                 // package task.
                 dexTask.conventionMapping.libraries = {
-                    if (isMultiDex(config)) {
+                    if (config.isMultiDexEnabled()) {
                         return Collections.emptyList()
                     }
 
@@ -2112,6 +2108,9 @@ public abstract class BasePlugin {
         compileTask.plugin = this
 
         compileTask.source = variantData.getJavaSources()
+
+        compileTask.multiDexEnabled = config.isMultiDexEnabled()
+        compileTask.minSdkVersion = config.minSdkVersion.apiLevel
 
         // if the tested variant is an app, add its classpath. For the libraries,
         // it's done automatically since the classpath includes the library output as a normal
@@ -2248,7 +2247,7 @@ public abstract class BasePlugin {
                 return null
             }
             packageApp.conventionMapping.dexedLibraries = {
-                if (isMultiDex(config) && variantData.preDexTask != null) {
+                if (config.isMultiDexEnabled() && variantData.preDexTask != null) {
                     return project.fileTree(variantData.preDexTask.outputFolder).files
                 }
 

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/DependenciesImpl.java
@@ -21,9 +21,11 @@ import static com.android.SdkConstants.DOT_JAR;
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.build.gradle.BasePlugin;
+import com.android.build.gradle.internal.core.GradleVariantConfiguration;
 import com.android.build.gradle.internal.dependency.LibraryDependencyImpl;
 import com.android.build.gradle.internal.dependency.VariantDependencies;
 import com.android.build.gradle.internal.variant.BaseVariantData;
+import com.android.builder.core.AndroidBuilder;
 import com.android.builder.dependency.JarDependency;
 import com.android.builder.dependency.LibraryDependency;
 import com.android.builder.model.AndroidLibrary;
@@ -112,8 +114,18 @@ public class DependenciesImpl implements Dependencies, Serializable {
                             jarDep.getResolvedCoordinates()));
         }
 
-        if (variantData.getVariantConfiguration().getRenderscriptSupportMode()) {
-            File supportJar = basePlugin.getAndroidBuilder().getRenderScriptSupportJar();
+        GradleVariantConfiguration variantConfig = variantData.getVariantConfiguration();
+        AndroidBuilder androidBuilder = basePlugin.getAndroidBuilder();
+
+        if (variantConfig.getRenderscriptSupportMode()) {
+            File supportJar = androidBuilder.getRenderScriptSupportJar();
+            if (supportJar != null) {
+                javaLibraries.add(new JavaLibraryImpl(supportJar, null, null));
+            }
+        }
+
+        if (variantConfig.isLegacyMultiDexMode()) {
+            File supportJar = androidBuilder.getMultiDexSupportJar();
             if (supportJar != null) {
                 javaLibraries.add(new JavaLibraryImpl(supportJar, null, null));
             }