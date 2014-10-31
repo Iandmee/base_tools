tadfisher/platform_tools_base

Add pseudolocalize flag into buildType configuration.
When this flag is set, two pseudolocales, en-XA and ar-XB are
merged into build resource configuration. The goal is create build
variants targeted to localization and internationalizaion testing.

Change-Id: Ie1db80663442b4d604eeabc272db4742c051c538

build-system/builder-model/src/main/java/com/android/builder/model/BuildType.java
@@ -54,6 +54,13 @@ public interface BuildType extends BaseConfig {
     boolean isTestCoverageEnabled();
 
     /**
+     * Returns whether the build type is configured to be build with support for pseudolocales.
+     *
+     * @return true if code coverage is enabled.
+     */
+    boolean isPseudolocalize();
+
+    /**
      * Returns whether the build type is configured to generate an apk with debuggable native code.
      *
      * @return true if the apk is debuggable

Iandmee/base_tools

(cherry picked from commit 4785b9cdf51d0a6da0f0c6b2b71e4055df540cc7)
Change-Id: I46acd6d1ba88f990fc1e5584d5474ba70159564d

build-system/builder-model/src/main/java/com/android/builder/model/BuildType.java
@@ -54,6 +54,13 @@ public interface BuildType extends BaseConfig {
     boolean isTestCoverageEnabled();
 
     /**
+     * Returns whether the build type is configured to be build with support for pseudolocales.
+     *
+     * @return true if code coverage is enabled.
+     */
+    boolean isPseudoLocalesEnabled();
+
+    /**
      * Returns whether the build type is configured to generate an apk with debuggable native code.
      *
      * @return true if the apk is debuggable