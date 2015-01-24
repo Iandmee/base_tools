tadfisher/platform_tools_base

Improve Truth support for zip files.
Change-Id: I714cc1cebf04c535fe2d8d8760713aef061af83d

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/DensitySplitTest.groovy
@@ -27,13 +27,16 @@ import com.android.builder.model.AndroidArtifactOutput
 import com.android.builder.model.AndroidProject
 import com.android.builder.model.Variant
 import com.google.common.collect.Maps
+import com.google.common.truth.Truth
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.ClassRule
 import org.junit.Test
 import org.junit.experimental.categories.Category
 
+import static com.android.build.gradle.integration.common.truth.ZipFileSubjectFactory.zipFile
 import static com.android.builder.core.BuilderConstants.DEBUG
+import static com.google.common.truth.Truth.assert_
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
@@ -76,9 +79,9 @@ class DensitySplitTest {
             assertEquals(5, mainArtifact.getOutputs().size())
 
             for (AndroidArtifactOutput output : mainArtifact.getOutputs()) {
-                ZipHelper.checkFileExists(
-                        output.getMainOutputFile().getOutputFile(),
-                        "res/drawable-mdpi-v4/other.png")
+                assert_().about(zipFile())
+                        .that(output.getMainOutputFile().getOutputFile())
+                        .contains("res/drawable-mdpi-v4/other.png")
             }
         }
     }

Iandmee/base_tools

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/DensitySplitTest.groovy
@@ -19,7 +19,6 @@ import com.android.build.OutputFile
 import com.android.build.gradle.integration.common.category.DeviceTests
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
 import com.android.build.gradle.integration.common.utils.ModelHelper
-import com.android.build.gradle.integration.common.utils.ZipHelper
 import com.android.builder.model.AndroidArtifact
 import com.android.builder.model.AndroidArtifactOutput
 import com.android.builder.model.AndroidProject
@@ -33,6 +32,7 @@ import org.junit.Test
 import org.junit.experimental.categories.Category
 
 import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThatApk
+import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThatZip
 import static com.android.builder.core.BuilderConstants.DEBUG
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertNotNull
@@ -76,9 +76,8 @@ class DensitySplitTest {
             assertEquals(5, mainArtifact.getOutputs().size())
 
             for (AndroidArtifactOutput output : mainArtifact.getOutputs()) {
-                ZipHelper.checkFileExists(
-                        output.getMainOutputFile().getOutputFile(),
-                        "res/drawable-mdpi-v4/other.png")
+                assertThatZip(output.getMainOutputFile().getOutputFile())
+                        .contains("res/drawable-mdpi-v4/other.png")
             }
         }
     }