tadfisher/platform_tools_base

Groovy to Java.
Change-Id: I3824161a5d84d97347f59766ae15e43cd63aeb50

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/tasks/CheckManifest.java
@@ -14,26 +14,44 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.tasks
+package com.android.build.gradle.internal.tasks;
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
 
 /**
  * Class that checks the presence of the manifest.
  */
 public class CheckManifest extends DefaultTask {
 
-    @InputFile
-    File manifest
+    private File mManifest;
 
-    String variantName
+    private String variantName;
+
+    @InputFile
+    public File getManifest() {
+        return mManifest;
+    }
+
+    public void setManifest(File manifest) {
+        mManifest = manifest;
+    }
+
+    public String getVariantName() {
+        return variantName;
+    }
+
+    public void setVariantName(String variantName) {
+        this.variantName = variantName;
+    }
 
     @TaskAction
     void check() {
         // use getter to resolve convention mapping
-        File f = getManifest()
+        File f = getManifest();
         if (!f.isFile()) {
             throw new IllegalArgumentException(
                     "Main Manifest missing for variant ${getVariantName()}. Expected path: ${f.getAbsolutePath()}");

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/tasks/DependencyReportTask.java
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.tasks;
+
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.AndroidAsciiReportRenderer;
+import com.android.build.gradle.internal.variant.BaseVariantData;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.logging.StyledTextOutputFactory;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ */
+public class DependencyReportTask extends DefaultTask {
+
+    private AndroidAsciiReportRenderer mRenderer = new AndroidAsciiReportRenderer();
+
+    private Set<BaseVariantData> mVariants = new HashSet<BaseVariantData>();
+
+    @TaskAction
+    public void generate() throws IOException {
+        mRenderer.setOutput(getServices().get(StyledTextOutputFactory.class).create(getClass()));
+
+        SortedSet<BaseVariantData> sortedConfigurations = new TreeSet<BaseVariantData>(
+                new Comparator<BaseVariantData>() {
+                    @Override
+            public int compare(BaseVariantData conf1, BaseVariantData conf2) {
+                return conf1.getName().compareTo(conf2.getName());
+            }
+        });
+        sortedConfigurations.addAll(getVariants());
+        for (BaseVariantData variant : sortedConfigurations) {
+            mRenderer.startVariant(variant);
+            mRenderer.render(variant);
+        }
+    }
+
+    /**
+     * Returns the configurations to generate the report for. Default to all configurations of
+     * this task's containing project.
+     *
+     * @return the configurations.
+     */
+    @NonNull
+    public Set<BaseVariantData> getVariants() {
+        return mVariants;
+    }
+
+    /**
+     * Sets the variants to generate the report for.
+     *
+     * @param variants the variants. Must not be null.
+     */
+    public void setVariants(@NonNull Collection<BaseVariantData> variants) {
+        this.mVariants.addAll(variants);
+    }
+
+    public void setVariants(
+            Set<BaseVariantData> variants) {
+        mVariants = variants;
+    }
+
+    @NonNull
+    public AndroidAsciiReportRenderer getRenderer() {
+        return mRenderer;
+    }
+
+    public void setRenderer(AndroidAsciiReportRenderer renderer) {
+        this.mRenderer = renderer;
+    }}

Iandmee/base_tools

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/tasks/CheckManifest.java
@@ -14,29 +14,50 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.tasks
+package com.android.build.gradle.internal.tasks;
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.TaskAction
+import com.android.annotations.NonNull;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
 
 /**
  * Class that checks the presence of the manifest.
  */
 public class CheckManifest extends DefaultTask {
 
-    @InputFile
-    File manifest
+    private File manifest;
 
-    String variantName
+    private String variantName;
+
+    @InputFile
+    public File getManifest() {
+        return manifest;
+    }
+
+    public void setManifest(@NonNull File manifest) {
+        this.manifest = manifest;
+    }
+
+    public String getVariantName() {
+        return variantName;
+    }
+
+    public void setVariantName(@NonNull String variantName) {
+        this.variantName = variantName;
+    }
 
     @TaskAction
     void check() {
         // use getter to resolve convention mapping
-        File f = getManifest()
+        File f = getManifest();
         if (!f.isFile()) {
-            throw new IllegalArgumentException(
-                    "Main Manifest missing for variant ${getVariantName()}. Expected path: ${f.getAbsolutePath()}");
+            throw new IllegalArgumentException(String.format(
+                    "Main Manifest missing for variant %s. Expected path: ",
+                    getVariantName(), getManifest().getAbsolutePath()));
         }
     }
 }

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/tasks/DependencyReportTask.java
@@ -14,19 +14,29 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.tasks
-import com.android.build.gradle.internal.AndroidAsciiReportRenderer
-import com.android.build.gradle.internal.variant.BaseVariantData
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
-import org.gradle.logging.StyledTextOutputFactory
-/**
- */
+package com.android.build.gradle.internal.tasks;
+
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.AndroidAsciiReportRenderer;
+import com.android.build.gradle.internal.variant.BaseVariantData;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.logging.StyledTextOutputFactory;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class DependencyReportTask extends DefaultTask {
 
     private AndroidAsciiReportRenderer renderer = new AndroidAsciiReportRenderer();
 
-    private Set<BaseVariantData> variants = [];
+    private Set<BaseVariantData> variants = new HashSet<BaseVariantData>();
 
     @TaskAction
     public void generate() throws IOException {
@@ -34,6 +44,7 @@ public class DependencyReportTask extends DefaultTask {
 
         SortedSet<BaseVariantData> sortedConfigurations = new TreeSet<BaseVariantData>(
                 new Comparator<BaseVariantData>() {
+                    @Override
             public int compare(BaseVariantData conf1, BaseVariantData conf2) {
                 return conf1.getName().compareTo(conf2.getName());
             }
@@ -56,11 +67,22 @@ public class DependencyReportTask extends DefaultTask {
     }
 
     /**
-     * Sets the configurations to generate the report for.
+     * Sets the variants to generate the report for.
      *
-     * @param configurations The configuration. Must not be null.
+     * @param variants the variants. Must not be null.
      */
-    public void setVariants(Collection<BaseVariantData> variants) {
+    public void setVariants(@NonNull Collection<BaseVariantData> variants) {
         this.variants.addAll(variants);
     }
-}
+
+    public void setVariants(Set<BaseVariantData> variants) {
+        this.variants = variants;
+    }
+
+    public AndroidAsciiReportRenderer getRenderer() {
+        return renderer;
+    }
+
+    public void setRenderer(@NonNull AndroidAsciiReportRenderer renderer) {
+        this.renderer = renderer;
+    }}