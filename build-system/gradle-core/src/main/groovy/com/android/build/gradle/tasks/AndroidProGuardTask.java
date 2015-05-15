tadfisher/platform_tools_base

runs all proguard/jack tasks through a limiter
Only runs the proguard/jack tasks through a limiter to limits the number
of concurrent proguard/jack invocations. Big projects have reported that
running many proguard/jack tasks can make the build fail or become
unbearably slow.

Change-Id: Ib9bd50a3b36a4e0f5d2ef55c38f5da711fa8876e

build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/AndroidProGuardTask.java
@@ -31,6 +31,8 @@ import com.android.build.gradle.internal.variant.BaseVariantData;
 import com.android.build.gradle.internal.variant.BaseVariantOutputData;
 import com.android.build.gradle.internal.variant.LibraryVariantData;
 import com.android.builder.core.VariantConfiguration;
+import com.android.builder.tasks.Job;
+import com.android.builder.tasks.JobContext;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
@@ -44,7 +46,6 @@ import org.gradle.tooling.BuildException;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -102,7 +103,32 @@ public class AndroidProGuardTask extends ProGuardTask implements FileSupplier {
 
     @Override
     @TaskAction
-    public void proguard() throws ParseException, IOException {
+    public void proguard() throws IOException, ParseException {
+        final Job<Void> job = new Job<Void>(getName(),
+                new com.android.builder.tasks.Task<Void>() {
+                    @Override
+                    public void run(@NonNull Job<Void> job,
+                            @NonNull JobContext<Void> context) throws IOException {
+                        try {
+                            AndroidProGuardTask.this.doMinification();
+                        } catch (ParseException e) {
+                            throw new IOException(e);
+                        }
+                    }
+                });
+        try {
+            MinificationWorkQueue.push(job);
+
+            // wait for the task completion.
+            job.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void doMinification() throws ParseException, IOException {
         // only set the tested application mapping file if it exists (it must at this point or that
         // means the tested application did not request obfuscation).
         if (testedAppMappingFile != null && testedAppMappingFile.exists()) {

Iandmee/base_tools

build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/AndroidProGuardTask.java
@@ -31,6 +31,8 @@ import com.android.build.gradle.internal.variant.BaseVariantData;
 import com.android.build.gradle.internal.variant.BaseVariantOutputData;
 import com.android.build.gradle.internal.variant.LibraryVariantData;
 import com.android.builder.core.VariantConfiguration;
+import com.android.builder.tasks.Job;
+import com.android.builder.tasks.JobContext;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
@@ -101,7 +103,32 @@ public class AndroidProGuardTask extends ProGuardTask implements FileSupplier {
 
     @Override
     @TaskAction
-    public void proguard() throws ParseException, IOException {
+    public void proguard() throws IOException, ParseException {
+        final Job<Void> job = new Job<Void>(getName(),
+                new com.android.builder.tasks.Task<Void>() {
+                    @Override
+                    public void run(@NonNull Job<Void> job,
+                            @NonNull JobContext<Void> context) throws IOException {
+                        try {
+                            AndroidProGuardTask.this.doMinification();
+                        } catch (ParseException e) {
+                            throw new IOException(e);
+                        }
+                    }
+                });
+        try {
+            SimpleWorkQueue.push(job);
+
+            // wait for the task completion.
+            job.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void doMinification() throws ParseException, IOException {
         // only set the tested application mapping file if it exists (it must at this point or that
         // means the tested application did not request obfuscation).
         if (testedAppMappingFile != null && testedAppMappingFile.exists()) {