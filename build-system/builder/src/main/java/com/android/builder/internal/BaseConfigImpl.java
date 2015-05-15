tadfisher/platform_tools_base

switched to jack/jill API 0.9.0
Change-Id: I2cbfb6f53a31f74c5a89e47befee3a765f99f9b5

build-system/builder/src/main/java/com/android/builder/internal/BaseConfigImpl.java
@@ -26,6 +26,7 @@ import com.google.common.collect.Maps;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -50,8 +51,8 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
     @Nullable
     private File mMultiDexKeepFile;
 
-    @Nullable
-    private File mJarJarRuleFile;
+    @NonNull
+    private Collection<File> mJarJarRuleFiles = Lists.newArrayList();
 
     /**
      * Adds a BuildConfig field.
@@ -193,7 +194,7 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
         mMultiDexKeepFile = that.getMultiDexKeepFile();
         mMultiDexKeepProguard = that.getMultiDexKeepProguard();
 
-        mJarJarRuleFile = that.getJarJarRuleFile();
+        mJarJarRuleFiles = that.getJarJarRuleFiles();
     }
 
     private void setBuildConfigFields(@NonNull Map<String, ClassField> fields) {
@@ -239,14 +240,14 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
         mMultiDexKeepProguard = file;
     }
 
-    public void setJarJarRuleFile(@Nullable File file) {
-        mJarJarRuleFile = file;
+    public void setJarJarRuleFiles(@NonNull List<File> files) {
+        mJarJarRuleFiles = files;
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public File getJarJarRuleFile() {
-        return mJarJarRuleFile;
+    public Collection<File> getJarJarRuleFiles() {
+        return mJarJarRuleFiles;
     }
 
     @Override
@@ -267,7 +268,8 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
                 Objects.equal(mMultiDexKeepFile, that.mMultiDexKeepFile) &&
                 Objects.equal(mMultiDexKeepProguard, that.mMultiDexKeepProguard) &&
                 Objects.equal(mProguardFiles, that.mProguardFiles) &&
-                Objects.equal(mResValues, that.mResValues);
+                Objects.equal(mResValues, that.mResValues) &&
+                Objects.equal(mJarJarRuleFiles, that.mJarJarRuleFiles);
 
     }
 
@@ -281,7 +283,8 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
                 mManifestPlaceholders,
                 mMultiDexEnabled,
                 mMultiDexKeepFile,
-                mMultiDexKeepProguard);
+                mMultiDexKeepProguard,
+                mJarJarRuleFiles);
     }
 
     @Override
@@ -295,6 +298,7 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
                 ", mMultiDexEnabled=" + mMultiDexEnabled +
                 ", mMultiDexKeepFile=" + mMultiDexKeepFile +
                 ", mMultiDexKeepProguard=" + mMultiDexKeepProguard +
+                ", mJarJarRuleFiles=" + mJarJarRuleFiles +
                 '}';
     }
 }

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/api/ReadOnlyBuildType.java
@@ -22,6 +22,7 @@ import com.android.builder.model.BuildType;
 import com.android.builder.model.SigningConfig;
 
 import java.io.File;
+import java.util.Collection;
 
 /**
  * Read-only version of the BuildType wrapping another BuildType.
@@ -115,9 +116,9 @@ public class ReadOnlyBuildType extends ReadOnlyBaseConfig implements BuildType {
         return readOnlyObjectProvider.getSigningConfig(buildType.getSigningConfig());
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public File getJarJarRuleFile() {
-        return buildType.getJarJarRuleFile();
+    public Collection<File> getJarJarRuleFiles() {
+        return buildType.getJarJarRuleFiles();
     }
 }

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/BaseConfigImpl.java
@@ -49,7 +49,7 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
     @Nullable
     private File mMultiDexKeepProguard;
     @Nullable
-    private File mJarJarRuleFile;
+    private Collection<File> mJarJarRuleFiles;
 
     protected BaseConfigImpl(@NonNull BaseConfig baseConfig) {
         mManifestPlaceholders = ImmutableMap.copyOf(baseConfig.getManifestPlaceholders());
@@ -58,7 +58,7 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
         mMultiDexEnabled = baseConfig.getMultiDexEnabled();
         mMultiDexKeepFile = baseConfig.getMultiDexKeepFile();
         mMultiDexKeepProguard = baseConfig.getMultiDexKeepProguard();
-        mJarJarRuleFile = baseConfig.getJarJarRuleFile();
+        mJarJarRuleFiles = baseConfig.getJarJarRuleFiles();
     }
 
     @NonNull
@@ -115,10 +115,10 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
         return mMultiDexKeepProguard;
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public File getJarJarRuleFile() {
-        return mJarJarRuleFile;
+    public Collection<File> getJarJarRuleFiles() {
+        return mJarJarRuleFiles;
     }
 
     @Override
@@ -128,7 +128,7 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
                 ", mBuildConfigFields=" + mBuildConfigFields +
                 ", mResValues=" + mResValues +
                 ", mMultiDexEnabled=" + mMultiDexEnabled +
-                ", mJarJarRuleFile=" + mJarJarRuleFile +
+                ", mJarJarRuleFiles=" + mJarJarRuleFiles +
                 '}';
     }
 }

Iandmee/base_tools

build-system/builder/src/main/java/com/android/builder/internal/BaseConfigImpl.java
@@ -26,6 +26,7 @@ import com.google.common.collect.Maps;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -50,8 +51,8 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
     @Nullable
     private File mMultiDexKeepFile;
 
-    @Nullable
-    private File mJarJarRuleFile;
+    @NonNull
+    private List<File> mJarJarRuleFiles = Lists.newArrayList();
 
     /**
      * Adds a BuildConfig field.
@@ -193,7 +194,7 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
         mMultiDexKeepFile = that.getMultiDexKeepFile();
         mMultiDexKeepProguard = that.getMultiDexKeepProguard();
 
-        mJarJarRuleFile = that.getJarJarRuleFile();
+        mJarJarRuleFiles = that.getJarJarRuleFiles();
     }
 
     private void setBuildConfigFields(@NonNull Map<String, ClassField> fields) {
@@ -239,14 +240,14 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
         mMultiDexKeepProguard = file;
     }
 
-    public void setJarJarRuleFile(@Nullable File file) {
-        mJarJarRuleFile = file;
+    public void setJarJarRuleFiles(@NonNull List<File> files) {
+        mJarJarRuleFiles = files;
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public File getJarJarRuleFile() {
-        return mJarJarRuleFile;
+    public List<File> getJarJarRuleFiles() {
+        return mJarJarRuleFiles;
     }
 
     @Override
@@ -267,7 +268,8 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
                 Objects.equal(mMultiDexKeepFile, that.mMultiDexKeepFile) &&
                 Objects.equal(mMultiDexKeepProguard, that.mMultiDexKeepProguard) &&
                 Objects.equal(mProguardFiles, that.mProguardFiles) &&
-                Objects.equal(mResValues, that.mResValues);
+                Objects.equal(mResValues, that.mResValues) &&
+                Objects.equal(mJarJarRuleFiles, that.mJarJarRuleFiles);
 
     }
 
@@ -281,7 +283,8 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
                 mManifestPlaceholders,
                 mMultiDexEnabled,
                 mMultiDexKeepFile,
-                mMultiDexKeepProguard);
+                mMultiDexKeepProguard,
+                mJarJarRuleFiles);
     }
 
     @Override
@@ -295,6 +298,7 @@ public abstract class BaseConfigImpl implements Serializable, BaseConfig {
                 ", mMultiDexEnabled=" + mMultiDexEnabled +
                 ", mMultiDexKeepFile=" + mMultiDexKeepFile +
                 ", mMultiDexKeepProguard=" + mMultiDexKeepProguard +
+                ", mJarJarRuleFiles=" + mJarJarRuleFiles +
                 '}';
     }
 }

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/api/ReadOnlyBuildType.java
@@ -22,6 +22,7 @@ import com.android.builder.model.BuildType;
 import com.android.builder.model.SigningConfig;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Read-only version of the BuildType wrapping another BuildType.
@@ -115,9 +116,9 @@ public class ReadOnlyBuildType extends ReadOnlyBaseConfig implements BuildType {
         return readOnlyObjectProvider.getSigningConfig(buildType.getSigningConfig());
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public File getJarJarRuleFile() {
-        return buildType.getJarJarRuleFile();
+    public List<File> getJarJarRuleFiles() {
+        return buildType.getJarJarRuleFiles();
     }
 }

build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/model/BaseConfigImpl.java
@@ -49,7 +49,7 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
     @Nullable
     private File mMultiDexKeepProguard;
     @Nullable
-    private File mJarJarRuleFile;
+    private List<File> mJarJarRuleFiles;
 
     protected BaseConfigImpl(@NonNull BaseConfig baseConfig) {
         mManifestPlaceholders = ImmutableMap.copyOf(baseConfig.getManifestPlaceholders());
@@ -58,7 +58,7 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
         mMultiDexEnabled = baseConfig.getMultiDexEnabled();
         mMultiDexKeepFile = baseConfig.getMultiDexKeepFile();
         mMultiDexKeepProguard = baseConfig.getMultiDexKeepProguard();
-        mJarJarRuleFile = baseConfig.getJarJarRuleFile();
+        mJarJarRuleFiles = baseConfig.getJarJarRuleFiles();
     }
 
     @NonNull
@@ -115,10 +115,10 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
         return mMultiDexKeepProguard;
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public File getJarJarRuleFile() {
-        return mJarJarRuleFile;
+    public List<File> getJarJarRuleFiles() {
+        return mJarJarRuleFiles;
     }
 
     @Override
@@ -128,7 +128,7 @@ abstract class BaseConfigImpl implements BaseConfig, Serializable {
                 ", mBuildConfigFields=" + mBuildConfigFields +
                 ", mResValues=" + mResValues +
                 ", mMultiDexEnabled=" + mMultiDexEnabled +
-                ", mJarJarRuleFile=" + mJarJarRuleFile +
+                ", mJarJarRuleFiles=" + mJarJarRuleFiles +
                 '}';
     }
 }