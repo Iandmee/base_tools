tadfisher/platform_tools_base

Make it harder to break variants API.
This adds tests that make sure you think twice before updating an
integration test that excercises our more programmatic APIs.

Change-Id: Ibb1d27a30b128b420742f41c8895158d631a25dd

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/ArtifactApiTest.groovy
@@ -28,6 +28,7 @@ import com.android.builder.model.ProductFlavorContainer
 import com.android.builder.model.SourceProvider
 import com.android.builder.model.SourceProviderContainer
 import com.android.builder.model.Variant
+import com.android.utils.FileUtils
 import groovy.transform.CompileStatic
 import org.junit.AfterClass
 import org.junit.BeforeClass
@@ -175,5 +176,13 @@ class ArtifactApiTest {
             assertNotNull("java artifact deps null-check", deps)
             assertFalse(deps.getJavaLibraries().isEmpty())
         }
+    }
+
+    @Test
+    public void backwardsCompatible() throws Exception {
+        // ATTENTION Author and Reviewers - please make sure required changes to the build file
+        // are backwards compatible before updating this test.
+        assertThat(FileUtils.sha1(project.file("build.gradle")))
+                .isEqualTo("ba48a2fe3da508ddcd95fc9d27dbb250f17c2079")
     }
 }

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/library/ApiTest.groovy
@@ -18,12 +18,15 @@ package com.android.build.gradle.integration.library
 
 import com.android.build.gradle.integration.common.category.DeviceTests
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.utils.FileUtils
 import groovy.transform.CompileStatic
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.ClassRule
 import org.junit.Test
 import org.junit.experimental.categories.Category
+
+import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat
 
 /**
  * Assemble tests for api.
@@ -54,5 +57,15 @@ class ApiTest {
     @Category(DeviceTests.class)
     void connectedCheck() {
         project.executeConnectedCheck()
+    }
+
+    @Test
+    public void backwardsCompatible() throws Exception {
+        // ATTENTION Author and Reviewers - please make sure required changes to the build file
+        // are backwards compatible before updating this test.
+        assertThat(FileUtils.sha1(project.file("app/build.gradle")))
+                .isEqualTo("61f5af1da4e8ee48360dbe16b50c81995b26e2bb")
+        assertThat(FileUtils.sha1(project.file("lib/build.gradle")))
+                .isEqualTo("f18801205a3f15490ce728c25c226d43db9d5342")
     }
 }

Iandmee/base_tools

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/ArtifactApiTest.groovy
@@ -28,6 +28,7 @@ import com.android.builder.model.ProductFlavorContainer
 import com.android.builder.model.SourceProvider
 import com.android.builder.model.SourceProviderContainer
 import com.android.builder.model.Variant
+import com.android.utils.FileUtils
 import groovy.transform.CompileStatic
 import org.junit.AfterClass
 import org.junit.BeforeClass
@@ -175,5 +176,13 @@ class ArtifactApiTest {
             assertNotNull("java artifact deps null-check", deps)
             assertFalse(deps.getJavaLibraries().isEmpty())
         }
+    }
+
+    @Test
+    public void backwardsCompatible() throws Exception {
+        // ATTENTION Author and Reviewers - please make sure required changes to the build file
+        // are backwards compatible before updating this test.
+        assertThat(FileUtils.sha1(project.file("build.gradle")))
+                .isEqualTo("cf6fa23a32f342718b1f342fc97846f56665a155")
     }
 }

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/library/ApiTest.groovy
@@ -18,12 +18,15 @@ package com.android.build.gradle.integration.library
 
 import com.android.build.gradle.integration.common.category.DeviceTests
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.utils.FileUtils
 import groovy.transform.CompileStatic
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.ClassRule
 import org.junit.Test
 import org.junit.experimental.categories.Category
+
+import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat
 
 /**
  * Assemble tests for api.
@@ -54,5 +57,15 @@ class ApiTest {
     @Category(DeviceTests.class)
     void connectedCheck() {
         project.executeConnectedCheck()
+    }
+
+    @Test
+    public void backwardsCompatible() throws Exception {
+        // ATTENTION Author and Reviewers - please make sure required changes to the build file
+        // are backwards compatible before updating this test.
+        assertThat(FileUtils.sha1(project.file("app/build.gradle")))
+                .isEqualTo("e20b70879b449c222ce9c0f9f17cb808d6899b06")
+        assertThat(FileUtils.sha1(project.file("lib/build.gradle")))
+                .isEqualTo("fbdd1f6d0d0e190412db885a1c281efee4ab0cac")
     }
 }