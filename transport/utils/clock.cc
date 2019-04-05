Flank/mirror-goog-studio-main

commit 3c47917e46d658f5b3e2fee451c5ec63829c8fc1

Minor refactor: removing int64->double->int64 conversion.
Test: manual
Bug: none

Change-Id: I39df76c4dcd3592003256c7bb1ea897174894161

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1e9 * time.tv_sec + time.tv_nsec;
+  return 1000000000 * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler

Iandmee/base_tools

commit 7db64a9ac2d03d53b0bb58967574ceb5d60ee5c0

Fix SteadyClock int overflow on 32-bit platforms
Broken by 3c47917e46d658f5b3e2fee451c5ec63829c8fc1

Bug: N/A
Test: N/A
Change-Id: I6d6697fbafb333d689b8795be020fbba0952b0f2

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1000000000 * time.tv_sec + time.tv_nsec;
+  return static_cast<int64_t>(1000000000) * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler