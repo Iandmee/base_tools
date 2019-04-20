Flank/mirror-goog-studio-main

commit 0f95d2524895e276d11fa5db0fcd180b916bb089

Separate Daemon and Agent configs
Currently, the daemon and agent shared the same "agent.config" even though
it was originally meant for the agent only. The same config file can be
overwritten and read multiple times (e.g. startup profiling) during the profiler
lifetime, so it is better to separate the non-overriddable configs (e.g. daemon)
from the overridable ones (e.g. agent). That way, we can better construct and push
configs for different agents at different times.

Bug: 129889001
Test: Existing

Change-Id: I25f10343269df2d87e20e90033119d20771dbb7e

transport/agent/transport_agent.cc
@@ -17,12 +17,12 @@
 #include "jvmti.h"
 
 #include <dlfcn.h>
+#include <fstream>
 #include <string>
 
 #include "agent/agent.h"
 #include "jvmti_helper.h"
 #include "perfa/perfa.h"
-#include "utils/config.h"
 #include "utils/device_info.h"
 #include "utils/log.h"
 
@@ -41,6 +41,13 @@ static std::string GetAppDataPath() {
   dladdr((void*)Agent_OnAttach, &dl_info);
   std::string so_path(dl_info.dli_fname);
   return so_path.substr(0, so_path.find_last_of('/') + 1);
+}
+
+static bool ParseConfigFromPath(const std::string& path, AgentConfig* config) {
+  std::fstream input(path, std::ios::in | std::ios::binary);
+  bool ret = config->ParseFromIstream(&input);
+  input.close();
+  return ret;
 }
 
 void LoadDex(jvmtiEnv* jvmti, JNIEnv* jni) {
@@ -66,9 +73,11 @@ extern "C" JNIEXPORT jint JNICALL Agent_OnAttach(JavaVM* vm, char* options,
 
   SetAllCapabilities(jvmti_env);
 
-  // TODO: Update options to support more than one argument if needed.
-  static const auto* const config = new profiler::Config(options);
-  auto const& agent_config = config->GetAgentConfig();
+  AgentConfig config;
+  if (!ParseConfigFromPath(options, &config)) {
+    Log::E("Failed to parse config from %s", options);
+    return JNI_ERR;
+  }
   Agent::Instance(config);
 
   JNIEnv* jni_env = GetThreadLocalJNI(vm);
@@ -78,12 +87,12 @@ extern "C" JNIEXPORT jint JNICALL Agent_OnAttach(JavaVM* vm, char* options,
   EchoAgentCommand::RegisterAgentEchoCommandHandler(vm);
 
   // Resource inspector agent.
-  if (agent_config.android_feature_level() >= DeviceInfo::Q) {
+  if (DeviceInfo::feature_level() >= DeviceInfo::Q) {
     LayoutInspectorAgentCommand::RegisterAgentLayoutInspectorCommandHandler(vm);
   }
 
   // Profiler agent.
-  SetupPerfa(vm, jvmti_env, agent_config);
+  SetupPerfa(vm, jvmti_env, config);
 
   Agent::Instance().AddDaemonConnectedCallback([] {
     Agent::Instance().StartHeartbeat();

profiler/native/agent/memory_component.cc
@@ -18,8 +18,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include "utils/config.h"
 #include "utils/log.h"
+#include "utils/socket_utils.h"
 #include "utils/thread_name.h"
 
 namespace profiler {
@@ -96,7 +96,7 @@ void MemoryComponent::SubmitMemoryTasks(
         do {
           // Each grpc call needs a new ClientContext.
           grpc::ClientContext ctx;
-          Config::SetClientContextTimeout(&ctx, kGrpcTimeoutSec);
+          SetClientContextTimeout(&ctx, kGrpcTimeoutSec);
           Status status = task(service_stub(), ctx);
           success = status.ok();
         } while (!success);

profiler/native/agent/support/cpu_trace.cc
@@ -111,7 +111,7 @@ void TraceMonitor::CheckFixTracePathCall(int32_t tid,
 }
 
 void TraceMonitor::SubmitStartEvent(int32_t tid, const string& fixed_path) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 
@@ -154,7 +154,7 @@ void TraceMonitor::SubmitStartEvent(int32_t tid, const string& fixed_path) {
 }
 
 void TraceMonitor::SubmitStopEvent(int tid) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 

profiler/native/agent/support/energy_passthrough.cc
@@ -112,7 +112,7 @@ const SteadyClock& GetClock() {
 // appropriate metadata must be set by the caller.
 void SubmitEnergyEvent(const EnergyEvent& energy_event,
                        const std::string& stack = {}) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 

profiler/native/agent/support/event_manager.cc
@@ -62,7 +62,7 @@ void EventManager::CacheAndEnqueueActivityEvent(
 
 void EventManager::EnqueueActivityEvent(
     const profiler::proto::SendActivityDataRequest& request) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 

profiler/native/agent/support/event_passthrough.cc
@@ -99,7 +99,7 @@ void SendKeyboardEvent(JStringWrapper& text, int64_t event_down_time) {
   int64_t timestamp = GetClock().GetCurrentTime();
   int32_t pid = getpid();
 
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     Agent::Instance().SubmitAgentTasks(
         {[pid, text, timestamp, event_down_time](AgentService::Stub& stub,
                                                  ClientContext& ctx) mutable {
@@ -128,7 +128,7 @@ void EnqueueActivityEvent(JNIEnv* env, const jstring& name,
   JStringWrapper activity_name(env, name);
   int64_t timestamp = GetClock().GetCurrentTime();
   int32_t pid = getpid();
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     bool is_end_state;
     switch (state) {
       case ViewData::PAUSED:
@@ -191,7 +191,7 @@ Java_com_android_tools_profiler_support_event_WindowProfilerCallback_sendTouchEv
   int64_t timestamp = GetClock().GetCurrentTime();
   int32_t pid = getpid();
 
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     Agent::Instance().SubmitAgentTasks(
         {[jdownTime, jstate, pid, timestamp, jis_up_event](
              AgentService::Stub& stub, ClientContext& ctx) mutable {
@@ -281,7 +281,7 @@ Java_com_android_tools_profiler_support_profilers_EventProfiler_sendRotationEven
   int64_t timestamp = GetClock().GetCurrentTime();
   int32_t pid = getpid();
 
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     Agent::Instance().SubmitAgentTasks({[jstate, pid, timestamp](
                                             AgentService::Stub& stub,
                                             ClientContext& ctx) mutable {

profiler/native/agent/support/http_tracker.cc
@@ -85,7 +85,10 @@ struct PayloadBuffer {
       // background thread to consume it. Additional bytes reported before the
       // background thread finally runs will be sent out at the same time.
       chunks[juid].push_back(bytes->get());
-      if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+      if (Agent::Instance()
+              .agent_config()
+              .common()
+              .profiler_unified_pipeline()) {
         Agent::Instance().SubmitAgentTasks(
             {[juid, this](AgentService::Stub &stub, ClientContext &ctx) {
               std::ostringstream batched_bytes;
@@ -208,7 +211,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024Connection_trac
     jlong jthread_id) {
   JStringWrapper thread_name(env, jthread_name);
 
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     int64_t timestamp = GetClock().GetCurrentTime();
     Agent::Instance().SubmitAgentTasks(
         {[timestamp, juid, thread_name, jthread_id](
@@ -245,7 +248,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024Connection_trac
 JNIEXPORT void JNICALL
 Java_com_android_tools_profiler_support_network_HttpTracker_00024InputStreamTracker_onClose(
     JNIEnv *env, jobject thiz, jlong juid) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     SendEventRequest request;
     PrepopulateEventRequest(&request, juid);
     std::stringstream ss;
@@ -290,7 +293,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024InputStreamTrac
 JNIEXPORT void JNICALL
 Java_com_android_tools_profiler_support_network_HttpTracker_00024InputStreamTracker_onReadBegin(
     JNIEnv *env, jobject thiz, jlong juid) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     // No-op. This is merged into onRequest.
   } else {
     EnqueueHttpEvent(juid, HttpEventRequest::DOWNLOAD_STARTED);
@@ -307,7 +310,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024InputStreamTrac
 JNIEXPORT void JNICALL
 Java_com_android_tools_profiler_support_network_HttpTracker_00024OutputStreamTracker_onClose(
     JNIEnv *env, jobject thiz, jlong juid) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     SendEventRequest request;
     PrepopulateEventRequest(&request, juid);
     std::stringstream ss;
@@ -359,7 +362,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024Connection_onRe
   JStringWrapper fields(env, jfields);
   JStringWrapper method(env, jmethod);
 
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     SendEventRequest request;
     PrepopulateEventRequest(&request, juid);
     Agent::Instance().SubmitAgentTasks({[request, url, stack, fields, method](
@@ -401,7 +404,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024Connection_onRe
     JNIEnv *env, jobject thiz, jlong juid, jstring jresponse, jstring jfields) {
   JStringWrapper fields(env, jfields);
 
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     SendEventRequest request;
     PrepopulateEventRequest(&request, juid);
     Agent::Instance().SubmitAgentTasks({[request, fields](
@@ -434,7 +437,7 @@ Java_com_android_tools_profiler_support_network_HttpTracker_00024Connection_onDi
 JNIEXPORT void JNICALL
 Java_com_android_tools_profiler_support_network_HttpTracker_00024Connection_onError(
     JNIEnv *env, jobject thiz, jlong juid, jstring jstatus) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     SendEventRequest request;
     PrepopulateEventRequest(&request, juid);
     Agent::Instance().SubmitAgentTasks(

profiler/native/agent/support/memory_stats_logger.cc
@@ -43,7 +43,7 @@ const SteadyClock& GetClock() {
 namespace profiler {
 
 void EnqueueAllocStats(int32_t alloc_count, int32_t free_count) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 
@@ -66,7 +66,7 @@ void EnqueueAllocStats(int32_t alloc_count, int32_t free_count) {
 }
 
 void EnqueueGcStats(int64_t start_time, int64_t end_time) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 
@@ -87,7 +87,7 @@ void EnqueueGcStats(int64_t start_time, int64_t end_time) {
 }
 
 void EnqueueAllocationEvents(proto::BatchAllocationSample& request) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 
@@ -100,7 +100,7 @@ void EnqueueAllocationEvents(proto::BatchAllocationSample& request) {
 }
 
 void EnqueueJNIGlobalRefEvents(proto::BatchJNIGlobalRefEvent& request) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 
@@ -114,7 +114,7 @@ void EnqueueJNIGlobalRefEvents(proto::BatchJNIGlobalRefEvent& request) {
 
 void EnqueueAllocationSamplingRateEvent(int64_t timestamp,
                                         int32_t sampling_num_interval) {
-  if (Agent::Instance().agent_config().profiler_unified_pipeline()) {
+  if (Agent::Instance().agent_config().common().profiler_unified_pipeline()) {
     return;
   }
 

Iandmee/base_tools

commit a01fdebcbc0d1d414c46f62c841efab65d936600

Fix an issue when the agent references a temporary copy of AgentConfig
Previously the AgentConfig that we passed around is a static instance.
0f95d2524895e276d11fa5db0fcd180b916bb089 inadvertently removed the static
so eventually the agent is referencing a invalid pointer on a separate
thread.

Bug: 130427589
Test: to follow
Change-Id: Ie9fbe239edab13e7c2186101f72ddf7fcfb37f64

transport/agent/transport_agent.cc
@@ -92,7 +92,10 @@ extern "C" JNIEXPORT jint JNICALL Agent_OnAttach(JavaVM* vm, char* options,
   }
 
   // Profiler agent.
-  SetupPerfa(vm, jvmti_env, config);
+  // We are passing in the AgentConfig stored in the Agent::Instance() because
+  // it will be accessed beyond the lifetime of this function. The |config|
+  // instance created within this function will no longer be valid.
+  SetupPerfa(vm, jvmti_env, Agent::Instance().agent_config());
 
   Agent::Instance().AddDaemonConnectedCallback([] {
     Agent::Instance().StartHeartbeat();